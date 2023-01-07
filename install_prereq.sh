94fccc3c50588483f4ca86c78fcc6a3233ce6453
InterLinked1 <24227567+InterLinked1@users.noreply.github.com>
2023-01-07 19:49:46 +0000

    install_prereq: Add libedit-dev.
    Adds libedit-dev for <histedit.h>

--- install_prereq.sh
+++ install_prereq.sh
@@ -9,6 +9,7 @@ apt-get install -y libcrypt-dev # crypt_r
 apt-get install -y libcurl4-openssl-dev # <curl/curl.h> - cURL, OpenSSL variant
 apt-get install -y binutils-dev # <bfd.h>
 apt-get install -y libcap-dev # <sys/capability.h>
+apt-get install -y libedit-dev # <histedit.h>
 
 # sock_ssh
 apt-get install -y libssh-dev # libssh
