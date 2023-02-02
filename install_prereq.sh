595118cd40966fff2270ff25229f5096fb971dcb
InterLinked1 <24227567+InterLinked1@users.noreply.github.com>
2023-02-02 10:56:56 +0000

    net_sftp: Add binutils to install_prereq.
    Our workaround to provide sftp_server_free
    for net_sftp if not available requires
    using objdump, which is usually available
    but not guaranteed to be. This is part of the
    binutils package, so install this as a prereq.

--- install_prereq.sh
+++ install_prereq.sh
@@ -11,8 +11,11 @@ apt-get install -y binutils-dev # <bfd.h>
 apt-get install -y libcap-dev # <sys/capability.h>
 apt-get install -y libedit-dev # <histedit.h>
 
-# sock_ssh
+# net_ssh and net_sftp
 apt-get install -y libssh-dev # libssh
+
+# net_sftp, which requires objdump to test for symbol existence... thanks a lot, libssh
+apt-get install -y binutils # binutils, for objdump
 
 # mod_mysql_auth
 apt-get install -y libmariadb-dev libmariadb-dev-compat # MariaDB (MySQL) dev headers
