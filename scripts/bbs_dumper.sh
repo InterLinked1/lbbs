46423851321f498a381ec253073d4309f7a5dbaf
InterLinked1 <24227567+InterLinked1@users.noreply.github.com>
2023-02-06 12:37:40 +0000

    bbs_dumper: Specify BBS binary path for livedump.
    The livedump backtrace can fail because we don't
    specify the path to the binary, so we now do that,
    just like with the postdump command.

--- scripts/bbs_dumper.sh
+++ scripts/bbs_dumper.sh
@@ -18,7 +18,7 @@ elif [ "$1" = "postdump" ]; then
 	gdb /usr/sbin/lbbs core -ex "thread apply all bt full" -ex "quit" > full.txt
 	printf "Backtrace saved to full.txt\n"
 elif [ "$1" = "livedump" ]; then
-	gdb --batch -q -p $bbspid -ex 'thread apply all bt full' -ex 'quit' > full.txt
+	gdb /usr/sbin/lbbs --batch -q -p $bbspid -ex 'thread apply all bt full' -ex 'quit' > full.txt
 	printf "Backtrace saved to full.txt\n"
 else
 	echo "Invalid command!"
