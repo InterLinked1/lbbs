7f5311d2d243db76b62d862f4ebc3ae5dec795db
InterLinked1 <24227567+InterLinked1@users.noreply.github.com>
2023-02-20 12:17:31 +0000

    bbs_dumper: Automatically install GDB if missing.
    To make it easier for end users who might need
    to run this script to get a backtrace, auto
    install GDB if we detect that it's not currently
    present.
    
    Using which to detect GDB is insufficient for...
    reasons. The logic here is stolen from this change:
    https://github.com/InterLinked1/phreakscript/commit/01a7c0ab310b380700f2cf75ed5a6704385086b0

--- scripts/bbs_dumper.sh
+++ scripts/bbs_dumper.sh
@@ -8,6 +8,17 @@ ps -aux | grep "lbbs" | grep -v "grep"
 bbspid=`cat /var/run/lbbs/bbs.pid`
 printf "BBS PID: %d\n" $bbspid
 
+ensure_gdb_installed() {
+	# For some reason, using which is not sufficient and will lead to things like: /usr/bin/gdb does not support python
+	# That's because gdb isn't really installed.
+	# Use a technique aside from which/path/binary detection to see if we find something we expect:
+	helplines=`gdb --help | grep "GDB manual" | wc -l`
+	if [ "$helplines" != "1" ]; then
+		printf "GDB does not appear to be currently installed, trying to install it now...\n"
+		apt-get install -y gdb
+	fi
+}
+
 if [ "$1" = "pid" ]; then
 	echo $bbspid
 elif [ "$1" = "term" ]; then
@@ -15,9 +26,11 @@ elif [ "$1" = "term" ]; then
 elif [ "$1" = "quit" ]; then
 	kill -3 $bbspid
 elif [ "$1" = "postdump" ]; then
+	ensure_gdb_installed
 	gdb /usr/sbin/lbbs core -ex "thread apply all bt full" -ex "quit" > full.txt
 	printf "Backtrace saved to full.txt\n"
 elif [ "$1" = "livedump" ]; then
+	ensure_gdb_installed
 	gdb /usr/sbin/lbbs --batch -q -p $bbspid -ex 'thread apply all bt full' -ex 'quit' > full.txt
 	printf "Backtrace saved to full.txt\n"
 else
