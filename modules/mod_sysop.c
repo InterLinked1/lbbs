InterLinked1/lbbs

commit 7c438cb2831f74f1a502c1b63460ee49cf0ebe41

gcc: Fix compiling with optimizations enabled.
This fixes compilation when compiling with optimizations
enabled using gcc.

The restrict attribute has also been added in several
pertinent places, to minimize pointer aliasing.

Finally, optimizations are now enabled by default.
-march=native and -mtune=native are left out for now,
since they confuse valgrind and cause it to abort.

modules/mod_sysop.c
@@ -222,7 +222,6 @@ static void *sysop_handler(void *varg)
 	char cmdbuf[256];
 	int res;
 	struct pollfd pfd;
-	const char *histentry = NULL;
 	int sysopfdin, sysopfdout;
 	struct sysop_fd *fds = varg;
 
@@ -260,6 +259,7 @@ static void *sysop_handler(void *varg)
 			continue;
 		}
 		if (pfd.revents & POLLIN) {
+			const char *histentry = NULL;
 			ssize_t bytes_read = read(sysopfdin, buf, sizeof(buf));
 			if (bytes_read <= 0) {
 				bbs_debug(5, "read returned %ld\n", bytes_read);

Makefile
@@ -13,7 +13,7 @@ BBSTOPDIR:=$(subst $(space),\$(space),$(CURDIR))
 export BBSTOPDIR		# Top level dir, used in subdirs' Makefiles
 
 CC		= gcc
-CFLAGS = -Wall -Werror -Wunused -Wextra -Wparentheses -Wconversion -Wdangling-else -Waggregate-return -Wchar-subscripts -Wdouble-promotion -Wmissing-include-dirs -Wuninitialized -Wunknown-pragmas -Wstrict-overflow -Wstringop-truncation -Wmissing-format-attribute -Wnull-dereference -Warray-bounds=1 -Wduplicated-branches -Wduplicated-cond -Wtrampolines -Wfloat-equal -Wdeclaration-after-statement -Wshadow -Wundef -Wunused-macros -Wcast-qual -Wcast-align -Wwrite-strings -Wjump-misses-init -Wlogical-op -Wstrict-prototypes -Wmissing-prototypes -Wmissing-declarations -Wredundant-decls -Wpacked -Wnested-externs -Winline -Wdisabled-optimization -Wstack-protector -std=gnu99 -pthread -O0 -g -fno-omit-frame-pointer -fstrict-aliasing -fdelete-null-pointer-checks -fwrapv -D_FORTIFY_SOURCE=2
+CFLAGS = -Wall -Werror -Wunused -Wextra -Wparentheses -Wconversion -Wdangling-else -Waggregate-return -Wchar-subscripts -Wdouble-promotion -Wmissing-include-dirs -Wuninitialized -Wunknown-pragmas -Wstrict-overflow -Wstringop-truncation -Wmissing-format-attribute -Wnull-dereference -Warray-bounds=1 -Wduplicated-branches -Wduplicated-cond -Wtrampolines -Wfloat-equal -Wdeclaration-after-statement -Wshadow -Wundef -Wunused-macros -Wcast-qual -Wcast-align -Wwrite-strings -Wjump-misses-init -Wlogical-op -Wstrict-prototypes -Wmissing-prototypes -Wmissing-declarations -Wredundant-decls -Wpacked -Wnested-externs -Winline -Wdisabled-optimization -Wstack-protector -Wno-unused-result -std=gnu99 -pthread -O3 -g -fno-omit-frame-pointer -fstrict-aliasing -fdelete-null-pointer-checks -fwrapv -D_FORTIFY_SOURCE=2
 EXE		= lbbs
 PREFIX	= /usr/local
 BINDIR	= $(PREFIX)/bin

bbs/ansi.c
@@ -24,7 +24,7 @@
 #include "include/ansi.h"
 #include "include/utils.h" /* use bbs_str_safe_print */
 
-int bbs_ansi_strip(const char *in, int inlen, char *out, size_t outlen, int *strippedlen)
+int bbs_ansi_strip(const char *restrict in, int inlen, char *restrict out, size_t outlen, int *restrict strippedlen)
 {
 	int outindex = 0;
 

bbs/base64.c
@@ -99,7 +99,7 @@ static int ochar(struct baseio *bio, int c, FILE *so, const char *endl)
 	return 1;
 }
 
-static int __base64_encode_file(FILE *inputfile, FILE *outputfile, const char *endl)
+static int __base64_encode_file(FILE *restrict inputfile, FILE *restrict outputfile, const char *endl)
 {
 	static const unsigned char dtable[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
 		'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
@@ -155,7 +155,7 @@ static int __base64_encode_file(FILE *inputfile, FILE *outputfile, const char *e
 	return 0;
 }
 
-int base64_encode_file(const char *filename, FILE *outputfile, const char *endl)
+int base64_encode_file(const char *restrict filename, FILE *restrict outputfile, const char *restrict endl)
 {
 	FILE *fi;
 	int res;
@@ -190,7 +190,7 @@ static const unsigned char decoding_table[256] = {
 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
 /*! \note Modified and amalgamated from https://stackoverflow.com/questions/342409/how-do-i-base64-encode-decode-in-c/64856489 */
-unsigned char *base64_decode(const unsigned char *data, int input_length, int *outlen)
+unsigned char *base64_decode(const unsigned char *restrict data, int input_length, int *restrict outlen)
 {
 	int i, j;
 	int output_length;
@@ -250,7 +250,7 @@ static char encoding_table[] =
 static int mod_table[] = {0, 2, 1};
 
 /*! \brief Based on https://stackoverflow.com/questions/342409/how-do-i-base64-encode-decode-in-c/6782480#6782480 */
-char *base64_encode(const char *data, int input_length, int *outlen)
+char *base64_encode(const char *restrict data, int input_length, int *restrict outlen)
 {
 	char *encoded_data;
 	int i, j, output_len;

bbs/bbs.c
@@ -878,6 +878,18 @@ int main(int argc, char *argv[])
 	}
 	CHECK_INIT(load_modules());
 
+#ifdef SILLY_OPTIMIZATIONS
+	/* Many modules (and the core) don't free their configs, and if a module never rereads the config,
+	 * this memory is sitting around for no reason. Free all the configs after startup finishes,
+	 * any rereads will require a new parse the first time, but after that, it will linger around.
+	 * This is a minor optimization to free a small amount of memory that may never be needed again.
+	 * XXX config.c returns configs (not locked) to modules, reference counting should be utilized
+	 * to prevent destroying a config that may be in use (previously not an issue, since only done at shutdown)
+	 * Ideally, if modules did this themselves (explicitly destroyed the config), that would be race free.
+	 */
+	bbs_configs_free_all();
+#endif
+
 	fully_started = 1;
 	bbs_verb(1, "%s\n", COLOR(COLOR_SUCCESS) "BBS is fully started" COLOR_RESET);
 	bbs_event_dispatch(NULL, EVENT_STARTUP);

bbs/config.c
@@ -28,6 +28,7 @@
 
 #include "include/linkedlists.h"
 #include "include/config.h"
+#include "include/utils.h"
 
 struct bbs_keyval {
 	char *key;

bbs/crypt.c
@@ -54,6 +54,7 @@ extern char *_crypt_gensalt_blowfish_rn(const char *prefix,
 #include <openssl/rand.h>
 
 #include "include/crypt.h"
+#include "include/utils.h"
 
 int bbs_rand_alnum(char *buf, size_t len)
 {

bbs/crypt_blowfish.c
@@ -418,12 +418,17 @@ static void BF_encode(char *dst, const BF_word *src, int size)
 	const unsigned char *sptr = (const unsigned char *)src;
 	const unsigned char *end = sptr + size;
 	unsigned char *dptr = (unsigned char *)dst;
-	
+
 	do {
 		unsigned int c1, c2;
 		c1 = *sptr++;
 		*dptr++ = BF_itoa64[c1 >> 2];
 		c1 = (c1 & 0x03) << 4;
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wstrict-overflow"
+		/* This code is known to work, and I didn't write it, so I'm not touching it, lest it break */
+		/* Note: This pragma seems to only work for compiling, not linking, with -flto,
+		 * so the bbs Makefile passes -Wno-strict-overflow to the linker */
 		if (sptr >= end) {
 			*dptr++ = BF_itoa64[c1];
 			break;
@@ -437,6 +442,7 @@ static void BF_encode(char *dst, const BF_word *src, int size)
 			*dptr++ = BF_itoa64[c1];
 			break;
 		}
+#pragma GCC diagnostic pop
 
 		c2 = *sptr++;
 		c1 |= c2 >> 6;

commit bc3a891c17fadbc5825b83147b619d1dbdd4dfcd

mod_sysop: Fix command history regression.
Partially reverts commit 7c438cb2831f74f1a502c1b63460ee49cf0ebe41,
which broke command history in mod_sysop.

modules/mod_sysop.c
@@ -261,6 +261,7 @@ static void *sysop_handler(void *varg)
 	int res;
 	struct pollfd pfd;
 	int sysopfdin, sysopfdout;
+	const char *histentry;
 	struct sysop_fd *fds = varg;
 
 	sysopfdin = fds->fdin;
@@ -285,6 +286,7 @@ static void *sysop_handler(void *varg)
 	pfd.fd = sysopfdin;
 	pfd.events = POLLIN | POLLPRI | POLLERR | POLLHUP | POLLNVAL;
 
+	histentry = NULL;
 	for (;;) {
 		pfd.revents = 0;
 		res = poll(&pfd, 1, -1);
@@ -297,7 +299,6 @@ static void *sysop_handler(void *varg)
 			continue;
 		}
 		if (pfd.revents & POLLIN) {
-			const char *histentry = NULL;
 			ssize_t bytes_read = read(sysopfdin, buf, sizeof(buf));
 			if (bytes_read <= 0) {
 				bbs_debug(5, "read returned %ld\n", bytes_read);