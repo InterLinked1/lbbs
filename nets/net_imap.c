016a790ebf747e14cdac11e1d54e3a7b01125aca
InterLinked1 <24227567+InterLinked1@users.noreply.github.com>
2023-06-21 00:04:07 +0000

    net_imap: Cache remote mailbox sizes if needed.
    This adds caching for the STATUS responses returned
    by remote IMAP servers that do not support RFC 8438
    STATUS=SIZE, which requires us to issue FETCH 1:*
    to calculate the size of each folder. This is very
    slow and inefficient (it can take several seconds
    or more on large mailboxes), so this operation should
    be avoided unless necessary. We now only do this
    if we don't have a cached size available to us or
    the mailbox has changed since the cached size was
    stored. This results in a significant speedup
    with many/large mailboxes.

--- nets/net_imap.c
+++ nets/net_imap.c
@@ -2237,6 +2237,93 @@ static int __attribute__ ((format (gnu_printf, 6, 7))) __imap_client_send_wait_r
 	return res;
 }
 
+static FILE *status_size_cache_file_load(struct imap_session *imap, const char *remotename, int write)
+{
+	char cache_dir[256];
+	char cache_file_name[534];
+	FILE *fp;
+
+	/* Put them in a subdirectory of the maildir, so it doesn't clutter up the maildir.
+	 * The name just has to NOT start with the hierarchy delimiter (or it would be a maildir) */
+	snprintf(cache_dir, sizeof(cache_dir), "%s/__cache", mailbox_maildir(imap->mymbox));
+	if (write && eaccess(cache_dir, R_OK) && mkdir(cache_dir, 0700)) {
+		bbs_error("mkdir(%s) failed: %s\n", cache_dir, strerror(errno));
+	}
+	snprintf(cache_file_name, sizeof(cache_file_name), "%s/.imapremote.size.%s.%s", cache_dir, imap->virtprefix, remotename);
+	/* Replace the remote delimiter with a period.
+	 * The actual character in this case isn't super important,
+	 * and it doesn't need to be our local delimiter (which is just a coincidence).
+	 * It just needs to be deterministic and make this path unique on the filesystem,
+	 * and it can't be / as that would indicate a subdirectory.
+	 * Period is just a good choice, for the same reason it's a good choice for the maildir++ delimiter. */
+	bbs_strreplace(cache_file_name, imap->virtdelimiter, '.');
+	fp = fopen(cache_file_name, write ? "w" : "r");
+	if (!fp) {
+		if (write) {
+			bbs_error("fopen(%s) failed: %s\n", cache_file_name, strerror(errno));
+		}
+		return NULL;
+	}
+	return fp;
+}
+
+static int status_size_cache_fetch(struct imap_session *imap, const char *remotename, const char *remote_status_resp, size_t *mb_size)
+{
+	char buf[256];
+	size_t curlen;
+	FILE *fp;
+	char *tmp;
+
+	fp = status_size_cache_file_load(imap, remotename, 0);
+	if (!fp) {
+		bbs_debug(9, "Cache file does not exist\n");
+		return -1; /* Not an error, just the cache file didn't exist (probably the first time) */
+	}
+	fgets(buf, sizeof(buf), fp);
+	fclose(fp);
+
+	curlen = strlen(remote_status_resp);
+
+	/* We subtract 2 as the last characters won't match.
+	 * In the raw response from the server, we'll have a ) at the end for end of response,
+	 * but in the cached version, we appended the SIZE so there'll be a space there e.g. " SIZE XXX" */
+	if (strncmp(remote_status_resp, buf, curlen - 2)) {
+		bbs_debug(6, "Cached size is outdated ('%.*s' !~= '%.*s')\n", (int) (curlen - 2), remote_status_resp, (int) (curlen - 2), buf);
+		return -1;
+	}
+
+	/* Reuse the SIZE from last time */
+	tmp = strstr(buf + curlen - 2, "SIZE ");
+	if (!tmp) {
+		bbs_warning("Cache file missing size?\n");
+	}
+	tmp += STRLEN("SIZE ");
+	if (strlen_zero(tmp)) {
+		bbs_warning("Cache file missing size?\n");
+		return -1;
+	}
+	*mb_size = (size_t) atol(tmp);
+	return 0;
+}
+
+static void status_size_cache_update(struct imap_session *imap, const char *remotename, const char *remote_status_resp)
+{
+	FILE *fp = status_size_cache_file_load(imap, remotename, 1);
+
+	/* Using a separate file for every single remote folder is... not very efficient.
+	 * But it's very easy to deal with, much easier than using a single file for all remote boxes,
+	 * especially if some require changes and some don't.
+	 * And to put it in perspective, it might add a few ms of overhead per mailbox (at most),
+	 * whereas the caching itself is potentially saving seconds, so the bar is already very low, so to speak,
+	 * and this is very easy to reason about and should be bug-free.
+	 */
+	if (!fp) {
+		return;
+	}
+	fprintf(fp, "%s\n", remote_status_resp);
+	fclose(fp);
+}
+
 static int remote_status(struct imap_session *imap, const char *remotename, const char *items, int size)
 {
 	char buf[1024];
@@ -2247,15 +2334,35 @@ static int remote_status(struct imap_session *imap, const char *remotename, cons
 	int len, res;
 	char *tmp;
 	struct bbs_tcp_client *client = &imap->client;
+	const char *add1, *add2, *add3;
 
 	client->rldata.buf = buf;
 	client->rldata.len = sizeof(buf);
+
+	/* In order for caching of SIZE to be reliable, we must invalidate it whenever anything
+	 * in the original STATUS response, including UIDNEXT/UIDVALIDITY. These are needed to
+	 * reliably detect changes to the mailbox (e.g. if a message is added and delete,
+	 * with MESSAGES/UNSEEN/RECENT alone, those might not change, but SIZE would probably
+	 * change if the added message is different from the deleted message.
+	 * In this case though, UIDNEXT and UIDVALIDITY would both change.
+	 * Likewise, UIDVALIDITY and UIDNEXT alone are NOT sufficient, because if a message is deleted,
+	 * UIDNEXT and UIDVALIDITY do not change (but MESSAGES will).
+	 * TL;DR - to detect changes, we need all three of MESSAGES, UIDNEXT, and UIDVALIDITY.
+	 *
+	 * Even if the client did not request them, if the remote does not support STATUS=SIZE,
+	 * request these (if not already requested).
+	 * These will also get returned in the response (even if the client didn't ask for them),
+	 * but that's not really an issue, since it's getting more than what it asked for.
+	 */
+	add1 = !(imap->virtcapabilities & IMAP_CAPABILITY_STATUS_SIZE) && !strstr(items, "UIDNEXT") ? " UIDNEXT" : "";
+	add2 = !(imap->virtcapabilities & IMAP_CAPABILITY_STATUS_SIZE) && !strstr(items, "UIDVALIDITY") ? " UIDVALIDITY" : "";
+	add3 = !(imap->virtcapabilities & IMAP_CAPABILITY_STATUS_SIZE) && !strstr(items, "MESSAGES") ? " MESSAGES" : "";
 
 	/* XXX The tag sent to the remote end will be the same for every single mailbox (for LIST-STATUS)
 	 * Since we're not pipelining these that's fine (and even if we were, it wouldn't be ambiguous
 	 * since the response contains the mailbox name), but this is certainly not a "proper" thing to do... */
 	taglen = (size_t) snprintf(rtag, sizeof(rtag), "A.%s.1 OK", imap->tag);
-	len = snprintf(buf, sizeof(buf), "A.%s.1 STATUS \"%s\" (%s)\r\n", imap->tag, remotename, items);
+	len = snprintf(buf, sizeof(buf), "A.%s.1 STATUS \"%s\" (%s%s%s%s)\r\n", imap->tag, remotename, items, add1, add2, add3);
 	imap_debug(3, "=> %.*s", len, buf);
 	bbs_write(client->wfd, buf, (size_t) len);
 	res = bbs_readline(client->rfd, &client->rldata, "\r\n", 5000);
@@ -2278,19 +2385,35 @@ static int remote_status(struct imap_session *imap, const char *remotename, cons
 
 	/*! \todo If something higher up is doing a LIST-STATUS, and the remote server supports LIST-STATUS,
 	 * do that (once), rather than doing a STATUS on each mailbox there
-	 * What we can do is keep a local temp file: issue LIST-STATUS once, store all the results,
-	 * and then when we enter this function for a specific mailbox, before issuing a STATUS query, check that file.
-	 *
-	 * The second optimization will be caching, important for servers that don't support STATUS=SIZE.
-	 * After each invocation of this function, we will store the STATUS response received from the server.
+	 * What we can do is keep a local cache: issue LIST-STATUS once, store all the results,
+	 * and then when we enter this function for a specific mailbox, before issuing a STATUS query, check that cache first.
+	 */
+
+	/*
+	 * The other optimization is caching the SIZE for servers that don't support STATUS=SIZE.
+	 * After each invocation of this function, we store the STATUS response received from the server.
 	 * If it is identical to the cached version, we know that SIZE must be the same, without issuing a FETCH 1:* (which is slow!)
-	 * To be 100% reliable, we should include UIDNEXT and UIDVALIDITY in the STATUS query,
+	 * To be 100% reliable, we include MESSAGES, UIDNEXT, and UIDVALIDITY in the STATUS query,
 	 * even if the client didn't ask for them (should be okay to respond with them regardless anyways).
+	 * In this case, we still have the overhead of doing STATUS for each folder, but that's peanuts compared to FETCH 1:*
 	 */
 
 	if (size && !strstr(remote_status_resp, "SIZE ")) { /* If we want SIZE and the server didn't send it, calculate it. */
 		size_t mb_size = 0;
-		if (!strstr(remote_status_resp, "MESSAGES 0")) { /* If we know the folder is empty, we know SIZE is 0 without asking */
+		int cached = 0;
+		/* Check the cache to see if we've already done a FETCH 1:* before and if the mailbox has changed since.
+		 * If not, the SIZE will be the same as last time, and we can just return that.
+		 * This is an EXTREMELY IMPORTANT optimization, because this operation can take a VERY long time on large mailboxes.
+		 * It can easily take a couple to several seconds - a rule of thumb might be 1 second per 10,000 messages.
+		 * It's a lot of data being received, a lot of calculations, and probably even more work for the poor IMAP server to compute.
+		 * Multiply this by a lot of mailboxes, and this can literally save time on the order of a minute.
+		 * (And given webmail clients are probably the most interested in SIZE, since they have no persistent state between sessions,
+		 *  speed matters the most there and nobody wants to wait a minute for a webmail client to load.)
+		 */
+		if (!status_size_cache_fetch(imap, remotename, remote_status_resp, &mb_size)) {
+			cached = 1;
+			bbs_debug(5, "Reusing previously cached SIZE: %lu\n", mb_size);
+		} else if (!strstr(remote_status_resp, "MESSAGES 0")) { /* If we know the folder is empty, we know SIZE is 0 without asking */
 			/* EXAMINE it so it's read only */
 			/* XXX This will reuse imap->tag for the tag here... which isn't ideal but should be accepted.
 			 * Since we're not pipelining our commands, it doesn't really matter anyways. */
@@ -2343,6 +2466,12 @@ static int remote_status(struct imap_session *imap, const char *remotename, cons
 		}
 		*tmp = '\0';
 		snprintf(tmp, sizeof(remote_status_resp) - (size_t) (tmp - remote_status_resp), " SIZE %lu)", mb_size);
+
+		if (!cached) {
+			/* Cache all of this for next time, so we don't have to issue a FETCH 1:* (which can be VERY slow)
+			 * unless the mailbox has been modified in some way. */
+			status_size_cache_update(imap, remotename, remote_status_resp);
+		}
 	}
 
 	/* Replace remote mailbox name with our name for it.
