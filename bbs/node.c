InterLinked1/lbbs

commit 70ec48399497a1ca249620a4dd729afb73d04fa7

thread.c: Simplify thread cancel and kill.
Calling pthread_cancel followed by pthread_kill
is a common sequence throughout the BBS for thread
cleanup, so this is refactored into a common
BBS function that emits warnings automatically
as appropriate.

Locking in pty.c is also adjusted in order to
fix a deadlock that could occur if the node was
locked waiting for the PTY master thread to exit,
but the PTY master thread was trying acquire the
node lock at the top of the loop.

bbs/node.c
@@ -230,6 +230,7 @@ struct bbs_node *__bbs_node_request(int fd, const char *protname, void *mod)
 		return NULL;
 	}
 	pthread_mutex_init(&node->lock, NULL);
+	pthread_mutex_init(&node->ptylock, NULL);
 	node->id = newnodenumber;
 	node->fd = fd;
 
@@ -282,6 +283,18 @@ int bbs_node_unlock(struct bbs_node *node)
 {
 	bbs_assert_exists(node);
 	return pthread_mutex_unlock(&node->lock);
+}
+
+int bbs_node_pty_lock(struct bbs_node *node)
+{
+	bbs_assert_exists(node);
+	return pthread_mutex_lock(&node->ptylock);
+}
+
+int bbs_node_pty_unlock(struct bbs_node *node)
+{
+	bbs_assert_exists(node);
+	return pthread_mutex_unlock(&node->ptylock);
 }
 
 char bbs_node_input_translate(struct bbs_node *node, char c)
@@ -466,8 +479,7 @@ static void node_shutdown(struct bbs_node *node, int unique)
 	}
 
 	if (node->ptythread) {
-		pthread_cancel(node->ptythread);
-		pthread_kill(node->ptythread, SIGURG);
+		bbs_pthread_cancel_kill(node->ptythread);
 		bbs_pthread_join(node->ptythread, NULL); /* Wait for the PTY master thread to exit, and then clean it up. */
 		if (node->spy) {
 			/* The sysop was spying on this node when it got disconnected.
@@ -529,6 +541,7 @@ static void node_free(struct bbs_node *node)
 	bbs_verb(3, "Node %d has exited\n", node->id);
 	bbs_node_unlock(node);
 	pthread_mutex_destroy(&node->lock);
+	pthread_mutex_destroy(&node->ptylock);
 	free(node);
 }
 
@@ -1170,11 +1183,16 @@ static int node_handler_term(struct bbs_node *node)
 		bbs_debug(5, "Exiting\n");
 		return -1;
 	}
+
 	/* Set up the psuedoterminal */
+	bbs_node_lock(node); /* Lock to prevent node thread from being cancelled while it's registering itself. */
 	if (bbs_pty_allocate(node)) {
+		bbs_node_unlock(node);
 		bbs_debug(5, "Exiting\n");
 		return -1;
 	}
+	bbs_node_unlock(node);
+
 	if (defaultbps) {
 		/* If there's a default speed to emulate, set it */
 		bbs_node_set_speed(node, defaultbps);

bbs/pty.c
@@ -272,6 +272,7 @@ int bbs_node_spy(int fdin, int fdout, int nodenum)
 	SWRITE(fdout, COLOR_RESET); /* Reset color too, just in case */
 	SWRITE(fdout, TERM_CLEAR);
 
+	bbs_node_pty_lock(node);
 	node->spy = 1;
 	node->spyfd = fdout;
 	node->spyfdin = fdin;
@@ -279,6 +280,7 @@ int bbs_node_spy(int fdin, int fdout, int nodenum)
 		bbs_sigint_set_alertpipe(spy_alert_pipe);
 	}
 	bbs_fd_unbuffer_input(fdin, 0); /* Unbuffer input, so that sysop can type on the node's TTY in real time, not just flushed after line breaks. */
+	bbs_node_pty_lock(node);
 	bbs_node_unlock(node); /* We're done with the node. */
 
 	bbs_verb(3, "Spying begun on node %d\n", nodenum);
@@ -465,7 +467,8 @@ void *pty_master(void *varg)
 		fds[0].events = fds[1].events = POLLIN;
 		fds[0].revents = fds[1].revents = 0;
 		numfds = 2;
-		bbs_node_lock(node);
+		/* Don't try to acquire the regular node lock since that would deadlock during a shutdown. */
+		bbs_node_pty_lock(node);
 		speed = node->speed;
 		spy = node->spy;
 		if (node->spy) {
@@ -486,7 +489,7 @@ void *pty_master(void *varg)
 			spyfdin = node->spyfdin;
 			spyfdout = node->spyfd;
 		}
-		bbs_node_unlock(node);
+		bbs_node_pty_unlock(node);
 
 		pres = poll(fds, numfds, -1);
 		pthread_testcancel();

bbs/logger.c
@@ -25,7 +25,7 @@
 #include <string.h>
 #include <dirent.h> /* use opendir, closedir */
 #include <time.h>
-#include <unistd.h> /* use gettid */
+#include <unistd.h>
 #include <sys/time.h> /* use gettimeofday */
 #include <unistd.h> /* use write */
 #include <linux/limits.h> /* use PATH_MAX */

bbs/thread.c
@@ -32,9 +32,17 @@
 #include "include/utils.h"
 #include "include/linkedlists.h"
 
+static __thread int my_tid = 0;
+
 int bbs_gettid(void)
 {
 	int tid;
+
+	/* If we've called this before, return the cached value to avoid a system call.
+	 * Since every call to __bbs_log calls bbs_gettid, this is an important optimization. */
+	if (my_tid) {
+		return my_tid;
+	}
 
 	/* We cannot merely have a macro defining gettid if it's not defined,
 	 * because the native return types of gettid and SYS_gettid differ.
@@ -47,6 +55,7 @@ int bbs_gettid(void)
 #else
 	tid = syscall(SYS_gettid);
 #endif
+	my_tid = tid; /* Save the value for future reference, so we don't need to do this again. */
 	return tid;
 }
 
@@ -223,10 +232,63 @@ int bbs_dump_threads(int fd)
 	return 0;
 }
 
+int bbs_pthread_cancel_kill(pthread_t thread)
+{
+	int res;
+
+	res = pthread_cancel(thread);
+	if (res) {
+		if (res == ESRCH) {
+			bbs_debug(3, "Thread %lu no longer exists\n", thread);
+		} else {
+			bbs_warning("Could not cancel thread %lu: %s\n", thread, strerror(res));
+		}
+	}
+
+	res = pthread_kill(thread, SIGURG);
+	if (res) {
+		if (res == ESRCH) {
+			bbs_debug(3, "Thread %lu no longer exists\n", thread);
+		} else {
+			bbs_warning("Could not kill thread %lu: %s\n", thread, strerror(res));
+		}
+	} else {
+		bbs_debug(3, "Killed thread %lu\n", thread);
+	}
+	return res;
+}
+
 int __bbs_pthread_join(pthread_t thread, void **retval, const char *file, const char *func, int line)
 {
 	void *tmp;
 	int res;
+	struct thread_list_t *x;
+	int lwp;
+
+	RWLIST_RDLOCK(&thread_list);
+	RWLIST_TRAVERSE(&thread_list, x, list) {
+		if (thread == x->id) {
+			lwp = x->lwp;
+			if (x->detached) {
+				bbs_error("Can't join detached LWP %d at %s:%d %s()\n", lwp, file, line, func);
+				lwp = 0;
+			} else if (!x->waitingjoin) {
+				/* This is suspicious... we may end up hanging if the thread doesn't exit imminently */
+				bbs_warning("Thread %d is not currently waiting to be joined\n", lwp);
+			}
+			break;
+		}
+	}
+	RWLIST_UNLOCK(&thread_list);
+
+	if (!x) {
+		bbs_error("Thread %lu not registered\n", thread);
+		return -1;
+	} else if (!lwp) {
+		return -1;
+	}
+
+	bbs_debug(6, "Attempting to join thread %lu (LWP %d) at %s:%d %s()\n", thread, lwp, file, line, func);
 	res = pthread_join(thread, retval ? retval : &tmp);
 	if (res) {
 		bbs_error("pthread_join(%lu) at %s:%d %s(): %s\n", thread, file, line, func, strerror(res));
@@ -235,7 +297,7 @@ int __bbs_pthread_join(pthread_t thread, void **retval, const char *file, const 
 	res = __thread_unregister(thread, file, line, func);
 	if (res == -1) {
 		bbs_error("Thread %d attempted to join nonjoinable thread %lu at %s:%d %s()\n", bbs_gettid(), thread, file, line, func);
-		return -1; /* pthread_join may have returned 0, but if the thread was detached, we probably can't trust its return value */
+		return -1; /* pthread_join may have returned 0, but if the thread was detached (though it can't be here!), we probably can't trust its return value */
 	}
 	return 0;
 }

include/node.h
@@ -44,6 +44,7 @@ struct bbs_node {
 	int menustack;				/*!< Current menu stack level */
 	char *ip;					/*!< IP Address */
 	pthread_mutex_t lock;		/*!< Node lock */
+	pthread_mutex_t ptylock;	/*!< Node PTY lock */
 	int created;				/*!< Creation time */
 	pid_t childpid;				/*!< Child PID of process node is currently exec'ing (0 if none) */
 	unsigned int bps;			/*!< Emulated terminal speed */
@@ -152,6 +153,12 @@ int bbs_node_lock(struct bbs_node *node);
 
 /*! Unlock a BBS node */
 int bbs_node_unlock(struct bbs_node *node);
+
+/*! Lock a BBS node for PTY operations */
+int bbs_node_pty_lock(struct bbs_node *node);
+
+/*! Unlock a BBS node for PTY operations */
+int bbs_node_pty_unlock(struct bbs_node *node);
 
 /*!
  * \brief Translate input characters from a node

include/utils.h
@@ -84,7 +84,12 @@ int bbs_user_identity_mismatch(struct bbs_user *user, const char *from);
 /*! \brief Get thread ID of current thread */
 int bbs_gettid(void);
 
+/*! \brief Cancel and kill a thread */
+int bbs_pthread_cancel_kill(pthread_t thread);
+
 int __bbs_pthread_join(pthread_t thread, void **retval, const char *file, const char *func, int line);
+
+/*! \brief Join a non-detached thread */
 #define bbs_pthread_join(thread, retval) __bbs_pthread_join(thread, retval, __FILE__, __FUNCTION__, __LINE__)
 
 int __bbs_pthread_create_detached(pthread_t *thread, pthread_attr_t *attr, void *(*start_routine)(void *), void *data, const char *file, const char *func, int line, const char *start_fn);

nets/net_finger.c
@@ -162,8 +162,7 @@ static int load_module(void)
 
 static int unload_module(void)
 {
-	pthread_cancel(finger_listener_thread);
-	pthread_kill(finger_listener_thread, SIGURG);
+	bbs_pthread_cancel_kill(finger_listener_thread);
 	bbs_pthread_join(finger_listener_thread, NULL);
 	close_if(finger_socket);
 	bbs_unregister_network_protocol(finger_port);

nets/net_ftp.c
@@ -26,7 +26,6 @@
 #include <string.h>
 #include <unistd.h>
 #include <pthread.h>
-#include <signal.h> /* use pthread_kill */
 #include <sys/sendfile.h>
 #include <dirent.h>
 
@@ -847,8 +846,7 @@ static int unload_module(void)
 		bbs_unregister_network_protocol(ftp_port);
 		close_if(ftp_socket);
 		close_if(ftps_socket);
-		pthread_cancel(ftp_thread);
-		pthread_kill(ftp_thread, SIGURG);
+		bbs_pthread_cancel_kill(ftp_thread);
 		bbs_pthread_join(ftp_thread, NULL);
 	} else {
 		bbs_error("FTP socket already closed at unload?\n");

commit b791987148c1fa8f740ae7f2f309ce9ab2d1ce1a

pty.c: Fix broken node spying.
This fixes node spying, which has been broken for some time now:

* Fix deadlock created by double lock (instead of lock/unlock).
  This regression was introduced in commit 70ec48399497a1ca249620a4dd729afb73d04fa7.
* Don't cause the PTY master thread to abort when a remote sysop
  console spying on a node disconnects. Instead, just continue looping.
  This has likely been broken since the PTY code was first written.
* Disable logging to STDOUT for a sysop console before spying,
  to avoid logging interfering with the spy session. mod_sysop will
  restore this logging after spying ends, if applicable.

bbs/node.c
@@ -1436,12 +1436,16 @@ void *bbs_node_handler(void *varg)
 
 static int cli_spy(struct bbs_cli_args *a)
 {
+	int res;
 	int node = atoi(a->argv[1]);
 	if (node <= 0) {
 		bbs_dprintf(a->fdout, "Invalid node %s\n", a->argv[1]);
 		return -1;
 	}
-	return bbs_node_spy(a->fdin, a->fdout, (unsigned int) node);
+	bbs_cli_set_stdout_logging(a->fdout, 0);
+	res = bbs_node_spy(a->fdin, a->fdout, (unsigned int) node);
+	/* Let mod_sysop re-enable logging to stdout, if configured */
+	return res;
 }
 
 static int cli_user(struct bbs_cli_args *a)

bbs/pty.c
@@ -275,12 +275,18 @@ int bbs_node_spy(int fdin, int fdout, unsigned int nodenum)
 	node = bbs_node_get(nodenum); /* This returns node locked */
 	if (!node) {
 		bbs_dprintf(fdout, "No such node: %u\n", nodenum);
+		if (fgconsole) {
+			bbs_alertpipe_close(spy_alert_pipe);
+		}
 		return 0;
 	}
 
 	if (!NODE_HAS_PTY(node)) {
 		bbs_dprintf(fdout, "Node %d does not have a PTY attached (protocol: %s)\n", node->id, node->protname);
 		bbs_node_unlock(node);
+		if (fgconsole) {
+			bbs_alertpipe_close(spy_alert_pipe);
+		}
 		return 0;
 	}
 
@@ -299,7 +305,7 @@ int bbs_node_spy(int fdin, int fdout, unsigned int nodenum)
 		bbs_sigint_set_alertpipe(spy_alert_pipe);
 	}
 	bbs_unbuffer_input(fdin, 0); /* Unbuffer input, so that sysop can type on the node's TTY in real time, not just flushed after line breaks. */
-	bbs_node_pty_lock(node);
+	bbs_node_pty_unlock(node);
 	bbs_node_unlock(node); /* We're done with the node. */
 
 	bbs_verb(3, "Spying begun on node %d\n", nodenum);
@@ -350,7 +356,8 @@ int bbs_node_spy(int fdin, int fdout, unsigned int nodenum)
 				break;
 			}
 		} while (res >= 0); /* poll should not return 0, since we passed -1 for ms */
-		/* Remote console socket is now closed */
+		/* Remote console socket is now closed.
+		 * Because it's closed from rsysop towards the BBS, we can't reset the terminal here. */
 	}
 
 	/* Log this message after the screen was cleared, as the sysop will likely see it,
@@ -359,6 +366,7 @@ int bbs_node_spy(int fdin, int fdout, unsigned int nodenum)
 
 	node = bbs_node_get(nodenum);
 	if (!node) {
+		bbs_debug(3, "Node %d disappeared while we were spying on it\n", nodenum);
 		return 0;
 	}
 	node->spy = 0;
@@ -623,7 +631,7 @@ gotinput:
 			/* Don't relay user input to sysop for spying here. If we're supposed to, it'll get echoed back in the output. */
 			if (bytes_wrote != bytes_read) {
 				bbs_error("Expected to write %ld bytes, only wrote %ld\n", bytes_read, bytes_wrote);
-				return NULL;
+				break;
 			}
 			if (bytes_read == 1 && *buf >= 1 && *buf <= 26) {
 				/* In general, we should not intercept messages between 1 and 26 (^A through ^Z).
@@ -759,7 +767,7 @@ finishoutput:
 			bytes_read = read(spyfdin, readbuf, sizeof(readbuf));
 			if (bytes_read <= 0) {
 				bbs_debug(10, "pty spy_in read returned %ld (%s)\n", bytes_read, strerror(errno));
-				break; /* We'll read 0 bytes upon disconnect */
+				continue; /* We'll read 0 bytes upon disconnect */
 			}
 #ifdef DEBUG_PTY
 			if (isprint(*buf)) {
@@ -783,7 +791,7 @@ finishoutput:
 				bytes_wrote = write(amaster, buf, (size_t) bytes_read);
 				if (bytes_wrote != bytes_read) {
 					bbs_error("Expected to write %ld bytes, only wrote %ld\n", bytes_read, bytes_wrote);
-					return NULL;
+					break;
 				}
 			} else {
 				bbs_warning("Got spy input for node %d, but not a spy target?\n", nodeid);