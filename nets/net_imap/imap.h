InterLinked1/lbbs

commit 32c6323fda24add7f899fdf8c175a10bad501869

io_compress, net_imap: Add support for DEFLATE compression.
* io_compress: Add I/O module for RFC 1951 DEFLATE compression.
* net_imap: Add RFC 4978 COMPRESS=DEFLATE support on top of that.

nets/net_imap/imap.h
@@ -166,7 +166,8 @@ extern int imap_debug_level;
 
 #define __imap_parallel_reply(imap, fmt, ...) imap_debug(4, "%p <= " fmt, imap, ## __VA_ARGS__); bbs_node_any_fd_writef(imap->node, imap->node->wfd, fmt, ## __VA_ARGS__);
 
-#define _imap_reply_nolock(imap, fmt, ...) imap_debug(4, "%p <= " fmt, imap, ## __VA_ARGS__); bbs_node_fd_writef(imap->node, imap->node->wfd, fmt, ## __VA_ARGS__);
+#define _imap_reply_nolock_fd(imap, fd, fmt, ...) imap_debug(4, "%p <= " fmt, imap, ## __VA_ARGS__); bbs_node_fd_writef(imap->node, fd, fmt, ## __VA_ARGS__);
+#define _imap_reply_nolock(imap, fmt, ...) imap_debug(4, "%p <= " fmt, imap, ## __VA_ARGS__); _imap_reply_nolock_fd(imap, imap->node->wfd, fmt, ## __VA_ARGS__);
 #define _imap_reply(imap, fmt, ...) _imap_reply_nolock(imap, fmt, ## __VA_ARGS__)
 #define imap_send(imap, fmt, ...) _imap_reply(imap, "%s " fmt "\r\n", "*", ## __VA_ARGS__)
 #define imap_reply(imap, fmt, ...) _imap_reply(imap, "%s " fmt "\r\n", imap->tag, ## __VA_ARGS__)

bbs/io.c
@@ -176,11 +176,49 @@ static int io_transform_store(struct bbs_io_transformations *trans, struct bbs_i
 	return -1;
 }
 
+static int __bbs_io_transform_possible(struct bbs_io_transformations *trans, enum bbs_io_transform_type type, int warn)
+{
+	if (bbs_io_transform_active(trans, type)) {
+		if (warn) {
+			bbs_error("Transformation %d already active, declining to set up duplicate transformation\n", type);
+		}
+		return 0;
+	}
+
+	/* TLS compression is disabled, so we don't need to worry about rejecting TRANSFORM_DEFLATE_COMPRESSION
+	 * if that were already to be active (as normally, that would conflict). */
+
+	/* XXX Ideally, ordering constraints would be specified in the modules themselves,
+	 * but since this involves both of them, just put it here for now: */
+	if (type == TRANSFORM_TLS_ENCRYPTION) {
+		if (bbs_io_transform_active(trans, TRANSFORM_DEFLATE_COMPRESSION)) {
+			/* Since I/O transformations are pushed onto a stack of file descriptors, effectively,
+			 * but TLS must happen after compression, it is too late to begin encryption.
+			 * The current I/O transformation architecture doesn't really us to add transformations
+			 * underneath existing ones. */
+			if (warn) {
+				bbs_warning("Can't enable encryption after compression has already been enabled, enable encryption prior to compression instead\n");
+			}
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int bbs_io_transform_possible(struct bbs_io_transformations *trans, enum bbs_io_transform_type type)
+{
+	return __bbs_io_transform_possible(trans, type, 0);
+}
+
 int bbs_io_transform_setup(struct bbs_io_transformations *trans, enum bbs_io_transform_type type, enum bbs_io_transform_dir direction, int *rfd, int *wfd, const void *arg)
 {
 	int res;
 	void *data = NULL;
 	struct bbs_io_transformer *t;
+
+	if (!__bbs_io_transform_possible(trans, type, 1)) {
+		return -1;
+	}
 
 	RWLIST_RDLOCK(&transformers);
 	if (!io_transform_slots_free(trans)) {
@@ -223,6 +261,25 @@ int bbs_io_transform_setup(struct bbs_io_transformations *trans, enum bbs_io_tra
 	RWLIST_UNLOCK(&transformers);
 
 	return res;
+}
+
+int bbs_io_transform_active(struct bbs_io_transformations *trans, enum bbs_io_transform_type type)
+{
+	int i, active = 0;
+
+	RWLIST_RDLOCK(&transformers);
+	for (i = 0; i < MAX_IO_TRANSFORMS; i++) {
+		if (trans->transformations[i].data) {
+			struct bbs_io_transformer *t = trans->transformations[i].transformer;
+			if (t->type == type) {
+				active = 1;
+				break;
+			}
+		}
+	}
+	RWLIST_UNLOCK(&transformers);
+
+	return active;
 }
 
 int bbs_io_transform_query(struct bbs_io_transformations *trans, enum bbs_io_transform_type type, int query, void *data)

bbs/node.c
@@ -2094,7 +2094,7 @@ int bbs_node_starttls(struct bbs_node *node)
 		return 1;
 	}
 
-	res = bbs_io_transform_setup(&node->trans,  TRANSFORM_TLS_ENCRYPTION, TRANSFORM_SERVER, &node->rfd, &node->wfd, NULL);
+	res = bbs_io_transform_setup(&node->trans, TRANSFORM_TLS_ENCRYPTION, TRANSFORM_SERVER, &node->rfd, &node->wfd, NULL);
 	if (res) {
 		/* If TLS setup fails, it's probably garbage traffic and safe to penalize: */
 		if (node) {

include/io.h
@@ -18,7 +18,7 @@
 
 enum bbs_io_transform_type {
 	TRANSFORM_TLS_ENCRYPTION = 0,	/*!< TLS encryption/decryption */
-	TRANSFORM_ZLIB_COMPRESSION = 1,	/*!< zlib compression/decompression */
+	TRANSFORM_DEFLATE_COMPRESSION = 1,	/*!< zlib compression/decompression */
 };
 
 /* Number of transform types in above enum + a few more if we allow other kinds */
@@ -35,7 +35,9 @@ enum bbs_io_transform_dir {
 #define TRANSFORM_CLIENT (TRANSFORM_CLIENT_TX | TRANSFORM_CLIENT_RX)
 #define TRANSFORM_SERVER_CLIENT_TX_RX (TRANSFORM_SERVER_TX | TRANSFORM_SERVER_RX | TRANSFORM_CLIENT_TX | TRANSFORM_CLIENT_RX)
 
-#define TRANSFORM_QUERY_TLS_REUSE 0
+#define TRANSFORM_QUERY_TLS_REUSE 0 /* TRANSFORM_TLS_ENCRYPTION: Get whether SSL session was reused, using arg as output */
+#define TRANSFORM_QUERY_COMPRESSION_FLUSH 1 /* TRANSFORM_DEFLATE_COMPRESSION: Do a full flush (arg not used) */
+#define TRANSFORM_QUERY_SET_COMPRESSION_LEVEL 2 /* TRANSFORM_DEFLATE_COMPRESSION: Set compression level (0 = none, 9 = max, slowest), using arg as input */
 
 struct bbs_io_transformer;
 
@@ -74,6 +76,15 @@ int bbs_io_named_transformer_available(const char *name);
 int bbs_io_transformer_available(enum bbs_io_transform_type transform_type);
 
 /*!
+ * \brief Check whether a transformation is possible, given what transformations are already running
+ * \param trans
+ * \param type
+ * \retval 1 transformation allowed
+ * \retval 0 not allowed, and calling bbs_io_transform_setup will fail
+ */
+int bbs_io_transform_possible(struct bbs_io_transformations *trans, enum bbs_io_transform_type type);
+
+/*!
  * \brief Begin using a transformer. This action is permanent (only bbs_io_teardown_all_transformers will end transformation)
  * \param trans
  * \param type
@@ -84,6 +95,15 @@ int bbs_io_transformer_available(enum bbs_io_transform_type transform_type);
  * \retval 0 on success, -1 on failure
  */
 int bbs_io_transform_setup(struct bbs_io_transformations *trans, enum bbs_io_transform_type type, enum bbs_io_transform_dir direction, int *rfd, int *wfd, const void *arg);
+
+/*!
+ * \brief Check whether a transformation is currently active
+ * \param trans
+ * \param type
+ * \retval 1 if currently active, 0 if not
+ * \note For checking if TLS is active on a node, use node->secure instead
+ */
+int bbs_io_transform_active(struct bbs_io_transformations *trans, enum bbs_io_transform_type type);
 
 /*!
  * \brief Read and/or write a setting while a transformation is active
@@ -105,5 +125,6 @@ int bbs_io_transform_query(struct bbs_io_transformations *trans, enum bbs_io_tra
 void bbs_io_teardown_all_transformers(struct bbs_io_transformations *trans);
 
 #define ssl_available() (bbs_io_transformer_available(TRANSFORM_TLS_ENCRYPTION))
+#define deflate_compression_available() (bbs_io_transformer_available(TRANSFORM_DEFLATE_COMPRESSION))
 
 #endif /* _BBS_IO_TRANSFORM */

io/Makefile
@@ -20,6 +20,10 @@ $(DEPENDS):
 	@echo "  [LD] $^ -> $@"
 	$(CC) -shared -fPIC -o $(basename $^).so $^
 
+io_compress.so : io_compress.o
+	@echo "  [LD] $^ -> $@"
+	$(CC) -shared -fPIC -o $(basename $^).so $^ -lz
+
 io_tls.so : io_tls.o
 	@echo "  [LD] $^ -> $@"
 	$(CC) -shared -fPIC -o $(basename $^).so $^ -lssl -lcrypto

io/io_compress.c
@@ -1,0 +1,375 @@
+/*
+ * LBBS -- The Lightweight Bulletin Board System
+ *
+ * Copyright (C) 2024, Naveen Albert
+ *
+ * Naveen Albert <bbs@phreaknet.org>
+ *
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License Version 2. See the LICENSE file
+ * at the top of the source tree.
+ */
+
+/*! \file
+ *
+ * \brief RFC 1951 DEFLATE compression
+ *
+ * \author Naveen Albert <bbs@phreaknet.org>
+ */
+
+#include "include/bbs.h"
+
+#include <string.h>
+#include <poll.h>
+#include <pthread.h>
+#include <signal.h>
+
+#include <zlib.h>
+
+#include "include/module.h"
+#include "include/node.h"
+#include "include/linkedlists.h"
+#include "include/alertpipe.h"
+#include "include/utils.h"
+#include "include/cli.h"
+
+/* See https://www.zlib.net/manual.html#Constants */
+#define DEFAULT_COMPRESSION_LEVEL 6
+
+struct compress_data {
+	z_stream compressor_s;
+	z_stream decompressor_s;
+	z_streamp compressor;
+	z_streamp decompressor;
+	int rpfd[2];
+	int wpfd[2];
+	int level; /* Current compression level */
+	int orig_rfd; /* Not used */
+	int orig_wfd;
+	size_t sentbytes;
+	size_t sentbytes_comp;
+	size_t recvbytes;
+	size_t recvbytes_comp;
+	pthread_t thread;
+	unsigned int done:1;
+	RWLIST_ENTRY(compress_data) entry;
+};
+
+static RWLIST_HEAD_STATIC(compressors, compress_data);
+
+static ssize_t compress_and_send(struct compress_data *z, char *buf, size_t len)
+{
+	char output[BUFSIZ];
+
+	z->compressor->next_in = (Bytef*) buf;
+	z->compressor->avail_in = (uInt) len;
+
+#ifdef DEBUG_COMPRESSION
+	bbs_debug(9, "Compressing %lu bytes\n", len);
+#endif
+
+	do {
+		size_t comp_len;
+		int res;
+		ssize_t wres;
+
+		z->compressor->avail_out = sizeof(output);
+		z->compressor->next_out = (Bytef*) output;
+
+		res = deflate(z->compressor, Z_PARTIAL_FLUSH);
+		if (res < 0) {
+			bbs_error("deflate failed: %s\n", zError(res));
+			bbs_soft_assert(res != Z_STREAM_ERROR);
+			return -1;
+		}
+		comp_len = sizeof(output) - z->compressor->avail_out;
+		bbs_debug(9, "Deflated to %lu bytes\n", comp_len);
+		wres = bbs_write(z->orig_wfd, output, comp_len);
+		if (wres <= 0) {
+			return -1;
+		}
+
+		z->sentbytes_comp += comp_len;
+		/* We filled up the buffer, so need to keep looping to flush out */
+	} while (z->compressor->avail_out == 0);
+	bbs_assert(z->compressor->avail_in == 0);
+	z->sentbytes += len;
+	return (ssize_t) len;
+}
+
+static ssize_t decompress_and_deliver(struct compress_data *z, char *buf, size_t len)
+{
+	char output[BUFSIZ];
+	int zres;
+	size_t bytes = 0;
+
+#ifdef DEBUG_COMPRESSION
+	bbs_debug(9, "Deflating %lu bytes\n", len);
+#endif
+
+	z->decompressor->next_in = (Bytef*) buf;
+	z->decompressor->avail_in = (uInt) len;
+
+	do {
+		ssize_t wres;
+		size_t decomp_len;
+
+		z->decompressor->avail_out = sizeof(output);
+		z->decompressor->next_out = (Bytef*) output;
+
+		zres = inflate(z->decompressor, Z_NO_FLUSH);
+		if (zres < 0) {
+			bbs_error("inflate failed: %s\n", zError(zres));
+			bbs_soft_assert(zres != Z_STREAM_ERROR);
+			return -1;
+		}
+		decomp_len = sizeof(output) - z->decompressor->avail_out;
+		bbs_debug(9, "Inflated to %lu bytes\n", decomp_len);
+		if (decomp_len == 0) {
+			bbs_debug(7, "Couldn't decompress anything with received input\n");
+			/* XXX Hopefully don't get stuck by looping again if nothing follows */
+			continue;
+		}
+		wres = bbs_write(z->rpfd[1], output, decomp_len);
+		if (wres <= 0) {
+			return -1;
+		}
+		z->recvbytes += decomp_len;
+		bytes += decomp_len;
+		/* If we couldn't decompress all the data received into the output buffer,
+		 * keep processing */
+	} while (z->decompressor->avail_out == 0);
+	z->recvbytes_comp += len;
+	return (ssize_t) bytes;
+}
+
+static void *zlib_thread(void *varg)
+{
+	struct compress_data *z = varg;
+	struct pollfd pfds[2];
+
+	pfds[0].fd = z->wpfd[0]; /* Data that we need to compress and send along */
+	pfds[1].fd = z->orig_rfd; /* Data that we received that needs to be decompressed */
+	pfds[0].events = pfds[1].events = POLLIN | POLLPRI | POLLERR | POLLNVAL;
+	pfds[0].revents = pfds[1].revents = 0;
+
+	for (;;) {
+		ssize_t rres;
+		ssize_t res = poll(pfds, 2, -1);
+		if (res < 0) {
+			bbs_debug(3, "poll returned %ld: %s\n", res, strerror(errno));
+			if (errno == EINTR && !z->done) {
+				continue;
+			}
+			break;
+		}
+		if (pfds[0].revents & POLLIN) {
+			char input[BUFSIZ];
+			rres = read(z->wpfd[0], input, sizeof(input));
+			if (rres <= 0) {
+				break;
+			}
+			/* Compress it */
+			res = compress_and_send(z, input, (size_t) rres);
+			if (rres <= 0) {
+				break;
+			}
+			pfds[0].revents = 0;
+		}
+		if (pfds[1].revents & POLLIN) {
+			char input[BUFSIZ];
+			rres = read(z->orig_rfd, input, sizeof(input));
+			if (rres <= 0) {
+				break;
+			}
+			/* Decompress it */
+			res = decompress_and_deliver(z, input, (size_t) rres);
+			if (rres <= 0) {
+				break;
+			}
+			pfds[1].revents = 0;
+		}
+	}
+	bbs_debug(4, "zlib thread exiting\n");
+	PIPE_CLOSE(z->wpfd);
+	PIPE_CLOSE(z->rpfd);
+	return NULL;
+}
+
+/* I/O transformation callback functions */
+
+static int setup(int *rfd, int *wfd, enum bbs_io_transform_dir dir, void **restrict data, const void *arg)
+{
+	int res;
+	struct compress_data *z;
+
+	/* Regardless of whether we are the server or the client, we
+	 * compress data that we write, and decompress data that we receive.
+	 * So data written to wfd will be read by this module,
+	 * compressed, and written to the original wfd,
+	 * and data to the current rfd will be read by this module,
+	 * decompressed, and written to the original rfd.
+	 *
+	 * Compression must always happen FURTHER from the network socket than TLS,
+	 * so due to the architecture in play here (fiddling file descriptors around),
+	 * that means that compression is not compatible with encryption unless setup AFTERWARDS,
+	 * which is the most common flow, but is not guaranteed. */
+	UNUSED(dir);
+	UNUSED(arg);
+
+	z = calloc(1, sizeof(*z));
+	if (ALLOC_FAILURE(z)) {
+		return -1;
+	}
+
+	z->compressor = &z->compressor_s;
+	z->decompressor = &z->decompressor_s;
+	z->level = DEFAULT_COMPRESSION_LEVEL;
+
+	/* Some of the parameters (e.g. -15) are adapted in particular for the IMAP COMPRESS=DEFLATE extension,
+	 * as specified in RFC 4978 Section 4. */
+	z->compressor->zalloc = Z_NULL;
+	z->compressor->zfree = Z_NULL;
+	z->compressor->opaque = Z_NULL;
+	res = deflateInit2(z->compressor, z->level, Z_DEFLATED, -15, 8, Z_DEFAULT_STRATEGY);
+	if (res != Z_OK) {
+		bbs_error("Failed to initialize deflator\n");
+		free(z);
+		return -1;
+	}
+	z->compressor->avail_in = 0;
+	z->compressor->avail_out = 0;
+
+	z->decompressor->zalloc = Z_NULL;
+	z->decompressor->zfree = Z_NULL;
+	z->decompressor->opaque = Z_NULL;
+	res = inflateInit2(z->decompressor, -15);
+	if (res != Z_OK) {
+		bbs_error("Failed to initialize inflator\n");
+		deflateEnd(z->compressor);
+		free(z);
+		return -1;
+	}
+
+	if (pipe(z->rpfd)) {
+		bbs_error("pipe failed: %s\n", strerror(errno));
+		goto fail;
+	} else if (pipe(z->wpfd)) {
+		bbs_error("pipe failed: %s\n", strerror(errno));
+		goto fail;
+	}
+
+	/* Spawn a separate thread to handle the new intermediate layer and the compression/decompression */
+	if (bbs_pthread_create(&z->thread, NULL, zlib_thread, z)) {
+		goto fail;
+	}
+
+	/* Fiddle the file descriptors around */
+	z->orig_rfd = *rfd;
+	z->orig_wfd = *wfd;
+	*rfd = z->rpfd[0]; /* Application now reads from the read end of this pipe */
+	*wfd = z->wpfd[1]; /* Application now writes to the write end of the other pipe */
+
+	RWLIST_WRLOCK(&compressors);
+	RWLIST_INSERT_HEAD(&compressors, z, entry);
+	RWLIST_UNLOCK(&compressors);
+
+	*data = z; /* Store as transform callback data */
+	return 0;
+
+fail:
+	deflateEnd(z->compressor);
+	inflateEnd(z->decompressor);
+	PIPE_CLOSE(z->rpfd);
+	PIPE_CLOSE(z->wpfd);
+	free(z);
+	return -1;
+}
+
+static void cleanup(struct bbs_io_transformation *tran)
+{
+	struct compress_data *z = tran->data;
+	bbs_assert_exists(z);
+
+	RWLIST_WRLOCK(&compressors);
+	RWLIST_REMOVE(&compressors, z, entry);
+	RWLIST_UNLOCK(&compressors);
+
+	deflateEnd(z->compressor);
+	inflateEnd(z->decompressor);
+	z->done = 1;
+	pthread_kill(z->thread, SIGUSR1); /* Signal zlib_thread (shutdown(z->wpfd[1], SHUT_RDWR) does not work*/
+	PIPE_CLOSE(z->rpfd);
+	PIPE_CLOSE(z->wpfd);
+	bbs_pthread_join(z->thread, NULL);
+	free(z);
+}
+
+static int query(struct bbs_io_transformation *tran, int query, void *data)
+{
+	struct compress_data *z = tran->data;
+	int *result, comp_level;
+
+	switch (query) {
+	case TRANSFORM_QUERY_COMPRESSION_FLUSH:
+		bbs_debug(3, "Flushing compression dictionary\n");
+		return deflate(z->compressor, Z_FULL_FLUSH);
+	case TRANSFORM_QUERY_SET_COMPRESSION_LEVEL:
+		result = data;
+		comp_level = *result;
+		/* Valid compression level is 0 through 9 */
+		if (comp_level < Z_NO_COMPRESSION || comp_level > Z_BEST_COMPRESSION) {
+			bbs_error("Invalid compression level requested: %d\n", comp_level);
+			return -1;
+		}
+		if (z->level == comp_level) {
+			bbs_debug(5, "Compression level has not changed (%d)\n", z->level);
+			return 0;
+		}
+		z->level = comp_level;
+		bbs_debug(3, "Setting compression level to %d\n", z->level);
+		return deflateParams(z->compressor, z->level, Z_DEFLATED);
+	default:
+		bbs_warning("Unknown query type: %d\n", query);
+		return -1;
+	}
+	__builtin_unreachable();
+}
+
+static int cli_compression(struct bbs_cli_args *a)
+{
+	struct compress_data *z;
+
+	bbs_dprintf(a->fdout, "%3s %3s %8s %8s %5s %11s %11s %11s %11s %6s %6s %s\n", "RFD", "WFD", "Orig RFD", "Orig WFD", "Level", "Sent Data", "Sent (Comp)", "Recv Data", "Recv (Comp)", "Send %", "Recv %", "Thread");
+	RWLIST_RDLOCK(&compressors);
+	RWLIST_TRAVERSE(&compressors, z, entry) {
+		char send_pct[13], recv_pct[13];
+		snprintf(send_pct, sizeof(send_pct), "%d%%", (int) (100.0 * (double) z->sentbytes_comp / (double) z->sentbytes));
+		snprintf(recv_pct, sizeof(recv_pct), "%d%%", (int) (100.0 * (double) z->recvbytes_comp / (double) z->recvbytes));
+		bbs_dprintf(a->fdout, "%3d %3d %8d %8d %5d %11lu %11lu %11lu %11lu %6s %6s %lu\n", z->rpfd[0], z->wpfd[1], z->orig_rfd, z->orig_wfd, z->level, z->sentbytes, z->sentbytes_comp, z->recvbytes, z->recvbytes_comp, send_pct, recv_pct, z->thread);
+	}
+	RWLIST_UNLOCK(&compressors);
+	return 0;
+}
+
+static struct bbs_cli_entry cli_commands_compress[] = {
+	BBS_CLI_COMMAND(cli_compression, "compressions", 1, "List all compression sessions", NULL),
+};
+
+static int load_module(void)
+{
+	int res = bbs_io_transformer_register("DEFLATE", setup, query, cleanup, TRANSFORM_DEFLATE_COMPRESSION, TRANSFORM_SERVER_CLIENT_TX_RX);
+	if (res) {
+		return res;
+	}
+	bbs_cli_register_multiple(cli_commands_compress);
+	return 0;
+}
+
+static int unload_module(void)
+{
+	bbs_cli_unregister_multiple(cli_commands_compress);
+	return bbs_io_transformer_unregister("DEFLATE");
+}
+
+BBS_MODULE_INFO_STANDARD("RFC 1951 DEFLATE Compression");

io/io_tls.c
@@ -741,7 +741,7 @@ static SSL *ssl_client_new(int fd, int *rfd, int *wfd, const char *snihostname)
 		bbs_error("Failed to setup new SSL context\n");
 		return NULL;
 	}
-	SSL_CTX_set_options(ctx, SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3); /* Only use TLS */
+	SSL_CTX_set_options(ctx, SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3 | SSL_OP_NO_COMPRESSION); /* Only use TLS, disable compression */
 	ssl = SSL_new(ctx);
 	if (!ssl) {
 		bbs_error("Failed to create new SSL\n");
@@ -910,6 +910,12 @@ static SSL_CTX *tls_ctx_create(const char *cert, const char *key)
 		return NULL;
 	}
 
+	/* Disabling compression is a best practice to avoid attacks such as CRIME.
+	 * However, another reason we explicitly do so is to avoid conflicting with explicit compression,
+	 * such as DEFLATE. This cannot be enabled at the same time that compression is already enabled
+	 * at the TLS layer, so disabling it here ensures that attempts to enable explicit compression
+	 * don't need to worry about TLS compression already being enabled. */
+	SSL_CTX_set_options(ctx, SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3 | SSL_OP_NO_COMPRESSION); /* Only use TLS, disable compression */
 	SSL_CTX_set_verify(ctx, SSL_VERIFY_NONE, NULL); /* Server is not verifying the client, the client will verify the server */
 
 	if (SSL_CTX_use_certificate_chain_file(ctx, cert) <= 0) {

nets/net_imap.c
@@ -26,6 +26,7 @@
  * \note Supports RFC 4466, 4731 SEARCH extensions
  * \note Supports RFC 4467 URLAUTH (partially) for RFC 4468 BURL
  * \note Supports RFC 4959 SASL-IR
+ * \note Supports RFC 4978 COMPRESS=DEFLATE
  * \note Supports RFC 5032 WITHIN (OLDER, YOUNGER)
  * \note Supports RFC 5161 ENABLE
  * \note Supports RFC 5182 SEARCHRES
@@ -56,7 +57,6 @@
  * - RFC 3516, 4466 BINARY, RFC 5524 URLAUTH=BINARY
  * - RFC 4469 CATENATE
  * - RFC 4959 SASL-IR
- * - RFC 4978 COMPRESS=DEFLATE
  * - RFC 5255 LANGUAGE
  * - RFC 5257 ANNOTATE, RFC 5464 ANNOTATE (METADATA)
  * - RFC 5258 LIST extensions (obsoletes 3348)
@@ -88,6 +88,9 @@
 /* XXX IDLE is advertised here even if disabled (although if disabled, it won't work if a client tries to use it) */
 /* XXX URLAUTH is advertised so that SMTP BURL will function in Trojita, even though we don't need URLAUTH since we have a direct trust */
 #define IMAP_CAPABILITIES IMAP_REV " AUTH=PLAIN UNSELECT UNAUTHENTICATE SPECIAL-USE LIST-EXTENDED LIST-STATUS XLIST CHILDREN IDLE NOTIFY NAMESPACE QUOTA QUOTA=RES-STORAGE ID SASL-IR ACL SORT THREAD=ORDEREDSUBJECT THREAD=REFERENCES URLAUTH ESEARCH ESORT SEARCHRES UIDPLUS LITERAL+ MULTIAPPEND APPENDLIMIT MOVE WITHIN ENABLE CONDSTORE QRESYNC STATUS=SIZE"
+
+#define CAPABILITY_FMT "%s%s"
+#define CAPABILITY_ARGS IMAP_CAPABILITIES, deflate_compression_available() ? " COMPRESS=DEFLATE" : ""
 
 /* Capabilities advertised by popular mail providers, for reference/comparison, both pre and post authentication:
  * - Office 365
@@ -3666,10 +3669,10 @@ static int finish_auth(struct imap_session *imap, int auth)
 	 * As an optimization, we could save an RTT by sending them unsolicited */
 
 	if (auth) {
-		_imap_reply(imap, "%s OK [CAPABILITY %s] Success\r\n", imap->savedtag ? imap->savedtag : imap->tag, IMAP_CAPABILITIES); /* Use tag from AUTHENTICATE request */
+		_imap_reply(imap, "%s OK [CAPABILITY " CAPABILITY_FMT "] Success\r\n", imap->savedtag ? imap->savedtag : imap->tag, CAPABILITY_ARGS); /* Use tag from AUTHENTICATE request */
 		free_if(imap->savedtag);
 	} else {
-		imap_reply(imap, "OK [CAPABILITY %s] Login completed", IMAP_CAPABILITIES);
+		imap_reply(imap, "OK [CAPABILITY " CAPABILITY_FMT "] Login completed", CAPABILITY_ARGS);
 	}
 	return 0;
 }
@@ -4165,7 +4168,7 @@ static int imap_process(struct imap_session *imap, char *s)
 		 * However, in practice, the capabilities exposed by many servers
 		 * after authentication differ.
 		 */
-		imap_send(imap, "CAPABILITY " IMAP_CAPABILITIES);
+		imap_send(imap, "CAPABILITY " CAPABILITY_FMT, CAPABILITY_ARGS);
 		imap_reply(imap, "OK CAPABILITY completed");
 	} else if (!strcasecmp(command, "STARTTLS")) {
 		imap_reply(imap, "NO Explicit TLS not supported; use implicit TLS instead");
@@ -4572,6 +4575,38 @@ static int imap_process(struct imap_session *imap, char *s)
 			}
 		}
 		imap_reply(imap, "OK ENABLE completed."); /* Always reply OK, even if nonexistent capability. */
+	} else if (!strcasecmp(command, "COMPRESS")) {
+		REQUIRE_ARGS(s);
+		if (!strcasecmp(s, "DEFLATE")) {
+			if (!deflate_compression_available()) {
+				imap_reply(imap, "NO Compression unavailable");
+			} else if (bbs_io_transform_active(&imap->node->trans, TRANSFORM_DEFLATE_COMPRESSION)) {
+				imap_reply(imap, "NO [COMPRESSIONACTIVE] DEFLATE already enabled");
+			} else if (!bbs_io_transform_possible(&imap->node->trans, TRANSFORM_DEFLATE_COMPRESSION)) {
+				imap_reply(imap, "NO Can't enable compression");
+			} else {
+				/* Go ahead and enable it */
+				int orig_wfd = imap->node->wfd;
+				int err = bbs_io_transform_setup(&imap->node->trans, TRANSFORM_DEFLATE_COMPRESSION, TRANSFORM_SERVER, &imap->node->rfd, &imap->node->wfd, NULL);
+				if (err) {
+					imap_reply(imap, "NO Failed to enable compression");
+				} else {
+					/* We still need to reply with an OK before compression is active.
+					 * Since node->wfd has already been updated, manually write to the original file descriptor (which now sits post-compression).
+					 * Normally, this would be a bad idea, since we'd intersperse uncompressed data with compressed data managed by zlib,
+					 * but this should be before we've sent anything compressed. */
+					_imap_reply_nolock_fd(imap, orig_wfd, "%s OK DEFLATE active\r\n", imap->tag);
+					/*! \todo A possible optimization we could implement for compression sessions is mentioned in RFC 4978 at the end of Section 4,
+					 * namely flushing dictionary (using TRANSFORM_QUERY_COMPRESSION_FLUSH) before a literal,
+					 * then using TRANSFORM_QUERY_SET_COMPRESSION_LEVEL to set the compression level to 0 if we detect it's already DEFLATE-compressed
+					 * and restoring compression afterwards.
+					 * However, this would entail parsing email messages to determine exactly where attachments start while we're sending them,
+					 * in order to be able to do this, so it's a lot of work for possibly a marginal improvement... not high on my priority list right now. */
+				}
+			}
+		} else {
+			imap_reply(imap, "BAD Unknown compression method");
+		}
 	} else if (!strcasecmp(command, "TESTLOCK")) {
 		/* Hold the mailbox lock for a moment. */
 		/*! \note This is only used for the test suite, it is not part of any IMAP standard or intended for clients. */
@@ -4616,9 +4651,9 @@ static void handle_client(struct imap_session *imap)
 	}
 
 	if (bbs_user_is_registered(imap->node->user)) {
-		imap_send(imap, "PREAUTH [CAPABILITY %s] %s server logged in as %s", IMAP_CAPABILITIES, IMAP_REV, bbs_username(imap->node->user));
+		imap_send(imap, "PREAUTH [CAPABILITY " CAPABILITY_FMT "] %s server logged in as %s", CAPABILITY_ARGS, IMAP_REV, bbs_username(imap->node->user));
 	} else {
-		imap_send(imap, "OK [CAPABILITY %s] %s Service Ready", IMAP_CAPABILITIES, IMAP_REV);
+		imap_send(imap, "OK [CAPABILITY " CAPABILITY_FMT "] %s Service Ready", CAPABILITY_ARGS, IMAP_REV);
 	}
 
 	for (;;) {

commit f9d13996386ebcdc05410c70dc47415e6dfe3d5d

net_imap: Eliminate erroneous duplicate logging of commands/responses.
Fix minor regression introduced in 32c6323fda24add7f899fdf8c175a10bad501869,
which resulted in certain commands/responses getting logged twice.

nets/net_imap/imap.h
@@ -167,7 +167,7 @@ extern int imap_debug_level;
 #define __imap_parallel_reply(imap, fmt, ...) imap_debug(4, "%p <= " fmt, imap, ## __VA_ARGS__); bbs_node_any_fd_writef(imap->node, imap->node->wfd, fmt, ## __VA_ARGS__);
 
 #define _imap_reply_nolock_fd(imap, fd, fmt, ...) imap_debug(4, "%p <= " fmt, imap, ## __VA_ARGS__); bbs_node_fd_writef(imap->node, fd, fmt, ## __VA_ARGS__);
-#define _imap_reply_nolock(imap, fmt, ...) imap_debug(4, "%p <= " fmt, imap, ## __VA_ARGS__); _imap_reply_nolock_fd(imap, imap->node->wfd, fmt, ## __VA_ARGS__);
+#define _imap_reply_nolock(imap, fmt, ...) _imap_reply_nolock_fd(imap, imap->node->wfd, fmt, ## __VA_ARGS__);
 #define _imap_reply(imap, fmt, ...) _imap_reply_nolock(imap, fmt, ## __VA_ARGS__)
 #define imap_send(imap, fmt, ...) _imap_reply(imap, "%s " fmt "\r\n", "*", ## __VA_ARGS__)
 #define imap_reply(imap, fmt, ...) _imap_reply(imap, "%s " fmt "\r\n", imap->tag, ## __VA_ARGS__)

nets/net_imap/imap_client.c
@@ -886,7 +886,7 @@ static struct imap_client *__load_virtual_mailbox(struct imap_session *imap, con
 			 * Mind you, this is a shiny, brand new socket that we just created and successfully read from and wrote to!
 			 * This is pretty obvious code smell, but valgrind doesn't pick anything up,
 			 * and in theory we can do an fclose immediately too, so this works fine,
-			 * I'm not just not satisfied that I can't explain why we need to do this here.
+			 * I'm just not satisfied that I can't explain why we need to do this here.
 			 */
 			fclose(fp);
 