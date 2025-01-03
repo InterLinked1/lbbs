InterLinked1/lbbs

commit 5228300db18ca31588436d89c5d15795354856b7

net_imap: Properly wait for tagged response for proxied APPEND/MOVE.
Previously, we were not properly handling untagged responses
while waiting for the tagged response. Initially, not receiving
the tagged response immediately would cause failure, since we
were only checking if the first response read was expected.
The remote MOVE/COPY handler was updated previously to tolerate
untagged responses while waiting for the tagged response, but
these were not passed along to the client, as they should be.
A similar issue existed with APPENDs to a remote mailbox.

Now, in both cases, we properly wait for the tagged response, while
passing along everything received in the meantime to the client.

nets/net_imap.c
@@ -2622,15 +2622,12 @@ static int handle_append(struct imap_session *imap, char *s)
 		if (res < 0) {
 			return -1;
 		}
-		/* Relay response from remote, and no need to clean up */
-		res = bbs_readline(imap->client->client.rfd, &imap->client->client.rldata, "\r\n", SEC_MS(5));
-		if (res < 0) {
-			/* No need to clean up, since for remote server,
-			 * we're not appending seqnos/UIDs to a uintlist */
-			bbs_warning("APPEND tagged response not received from remote server?\n");
+		/* Relay response from remote, and no need to clean up.
+		 * We may (should) receive an untagged EXISTS for the uploaded message,
+		 * so use imap_client_wait_response to wait for the actual tagged response. */
+		if (imap_client_wait_response(imap->client, -1, SEC_MS(5))) {
 			return -1;
 		}
-		_imap_reply(imap, "%s\r\n", imap->client->client.buf);
 		return 0;
 	}
 
@@ -3035,13 +3032,16 @@ static int handle_remote_move(struct imap_session *imap, char *dest, const char 
 			if (SWRITE(destclient->client.wfd, "\r\n") != STRLEN("\r\n")) {
 				goto cleanup;
 			}
-			/* Well, hopefully that all worked!
-			 * Use IMAP_CLIENT_EXPECT_EVENTUALLY since we may also get untagged EXISTS's for the copied messages. */
-
-			/*! \todo BUGBUG For any IMAP_CLIENT_EXPECT_EVENTUALLY, all the untagged responses that we "ignore
-			 * should actually pass through to the client directly, since an offline client will need those
-			 * in order to be synchronized. */
-			IMAP_CLIENT_EXPECT_EVENTUALLY(&destclient->client, appended + 1, " OK"); /* tagged OK */
+			/* Well, hopefully that all worked! Now, wait for confirmation of a successful upload.
+			 *
+			 * Here, we use imap_client_wait_response instead of IMAP_CLIENT_EXPECT_EVENTUALLY to wait for the tagged OK,
+			 * because any untagged data we receive prior to that should pass through directly to the client
+			 * (rather than being silently ignored/discarded as IMAP_CLIENT_EXPECT_EVENTUALLY does).
+			 * This way, the client can stay synchronized with the remote server, which is important
+			 * since APPEND can trigger untagged EXISTS's, etc. */
+			if (imap_client_wait_response(destclient, -1, SEC_MS(5))) {
+				goto cleanup;
+			}
 		}
 		if (move) {
 			/* Now that we copied everything to the destination mailbox, delete the source */

nets/net_imap/imap_client.c
@@ -440,6 +440,7 @@ static ssize_t client_command_passthru(struct imap_client *client, int fd, const
 		}
 		res = bbs_readline(tcpclient->rfd, &tcpclient->rldata, "\r\n", ms);
 		if (res < 0) { /* Could include remote server disconnect */
+			bbs_warning("Failed to receive expected response from remote server\n");
 			return res;
 		}
 		if (cb) {
@@ -469,9 +470,13 @@ static ssize_t client_command_passthru(struct imap_client *client, int fd, const
 #endif
 		if (!strncmp(buf, tag, (size_t) taglen)) {
 			imap_debug(10, "<= %.*s\n", (int) res, buf);
-			if (STARTS_WITH(buf + taglen, "BAD")) {
+			if (!STARTS_WITH(buf + taglen, "OK")) { /* BAD or NO */
 				/* We did something we shouldn't have, oops */
-				bbs_warning("Command '%.*s%.*s' failed: %s\n", taglen, tag, cmdlen > 2 ? cmdlen - 2 : cmdlen, cmd, buf); /* Don't include trailing CR LF */
+				if (cmd) {
+					bbs_warning("Command '%.*s%.*s' failed: %s\n", taglen, tag, cmdlen > 2 ? cmdlen - 2 : cmdlen, cmd, buf); /* Don't include trailing CR LF */
+				} else {
+					bbs_warning("Received negatory response: %s\n", buf);
+				}
 			}
 			client->lastactive = time(NULL); /* Successfully just got data from remote server */
 			break; /* That's all, folks! */
@@ -508,6 +513,30 @@ ssize_t __imap_client_send_log(struct imap_client *client, int log, const char *
 	}
 
 	return bbs_write(client->client.wfd, buf, (size_t) len);
+}
+
+int __imap_client_wait_response(struct imap_client *client, int fd, int ms, int echo, int lineno, int (*cb)(struct imap_client *client, const char *buf, size_t len, void *cbdata), void *cbdata)
+{
+	char tagbuf[15];
+	int taglen;
+	const char *tag = "tag";
+
+	if (!client->imap) {
+		bbs_warning("No active IMAP client?\n"); /* Shouldn't happen... */
+		bbs_soft_assert(0);
+	} else if (strlen_zero(client->imap->tag)) {
+		bbs_warning("No active IMAP tag, using generic one\n");
+		bbs_soft_assert(0);
+	} else {
+		tag = client->imap->tag;
+	}
+
+	taglen = snprintf(tagbuf, sizeof(tagbuf), "%s ", tag); /* Reuse the tag the client sent us, so we can just passthrough the response */
+
+	UNUSED(lineno);
+
+	/* Read until we get the tagged respones */
+	return client_command_passthru(client, fd, tagbuf, taglen, NULL, 0, ms, echo, cb, cbdata) <= 0;
 }
 
 int __imap_client_send_wait_response(struct imap_client *client, int fd, int ms, int echo, int lineno, int (*cb)(struct imap_client *client, const char *buf, size_t len, void *cbdata), void *cbdata, const char *fmt, ...)

nets/net_imap/imap_client.h
@@ -86,11 +86,27 @@ ssize_t __attribute__ ((format (gnu_printf, 3, 4))) __imap_client_send_log(struc
 #define imap_client_send(client, fmt, ...) __imap_client_send_log(client, 0, fmt, ## __VA_ARGS__)
 #define imap_client_send_log(client, fmt, ...) __imap_client_send_log(client, 1, fmt, ## __VA_ARGS__)
 
+#define imap_client_wait_response(client, fd, ms) __imap_client_wait_response(client, fd, ms, 1, __LINE__, NULL, NULL)
+
 #define imap_client_send_wait_response(client, fd, ms, fmt, ...) __imap_client_send_wait_response(client, fd, ms, 1, __LINE__, NULL, NULL, fmt, ## __VA_ARGS__)
 #define imap_client_send_wait_response_cb(client, fd, ms, cb, cbdata, fmt, ...) __imap_client_send_wait_response(client, fd, ms, 1, __LINE__, cb, cbdata, fmt, ## __VA_ARGS__)
 #define imap_client_send_wait_response_noecho(client, fd, ms, fmt, ...) __imap_client_send_wait_response(client, fd, ms, 0, __LINE__, NULL, NULL, fmt, ## __VA_ARGS__)
 #define imap_client_send_wait_response_cb_noecho(client, fd, ms, cb, cbdata, fmt, ...) __imap_client_send_wait_response(client, fd, ms, 0, __LINE__, cb, cbdata, fmt, ## __VA_ARGS__)
 
+/*! \brief Same as __imap_client_send_wait_response, but don't send a command initially, just wait for the tagged response */
+int __imap_client_wait_response(struct imap_client *client, int fd, int ms, int echo, int lineno, int (*cb)(struct imap_client *client, const char *buf, size_t len, void *cbdata), void *cbdata);
+
+/*!
+ * \brief Send a command to remote IMAP server and wait for a response, passing through any receive untagged responses inbetween directly back to the client
+ * \param client
+ * \param fd Additional file descriptor on which to poll(used only for IDLE, -1 otherwise)
+ * \param ms Max time to wait for poll / bbs_readline
+ * \param echo Whether to relay output from remote server to our local client
+ * \param lineno
+ * \param cb Custom callback to run for each line received from remote server. Returning -1 from callback will terminate wait loop
+ * \param cbdata Callback data for callback function
+ * \param fmt printf-style fmt string
+ */
 int __attribute__ ((format (gnu_printf, 8, 9))) __imap_client_send_wait_response(struct imap_client *client, int fd, int ms, int echo, int lineno, int (*cb)(struct imap_client *client, const char *buf, size_t len, void *cbdata), void *cbdata, const char *fmt, ...);
 
 /*! \retval Number of replacements made */

commit 5800b617f25a627f36c04149882f9f182ced9bc4

net_imap: Avoid sending multiple tagged responses on cross-server COPY/MOVE.
A regression was introduced in commit 5228300db18ca31588436d89c5d15795354856b7,
which passes along all untagged responses received while waiting for
the tagged response from a remote server. This is generally an
improvement, since we want to pass the untagged responses along, but
not the tagged response in cases where we generate our own tagged
response. Doing so could cause clients like libetpan to lose
synchronization since they would receive a tagged response from the
remote server followed by the untagged response that we generate.

For these cases, pass through all untagged responses received, but not
the final tagged response from the remote server. Additional checks have
also been added to trigger assertions if multiple tagged responses are
sent to the client, to help catch any recurring instances of this bug.

nets/net_imap.c
@@ -3012,7 +3012,7 @@ static int handle_remote_move(struct imap_session *imap, char *dest, const char 
 						goto cleanup;
 					}
 					/* Could get an untagged EXISTS at this point */
-					if (imap_client_wait_response(destclient, -1, SEC_MS(5))) { /* tagged OK */
+					if (imap_client_wait_response_noechotag(destclient, -1, SEC_MS(5))) { /* wait for tagged OK, don't echo that, but echo everything else */
 						goto cleanup;
 					}
 				}
@@ -3040,8 +3040,10 @@ static int handle_remote_move(struct imap_session *imap, char *dest, const char 
 			 * because any untagged data we receive prior to that should pass through directly to the client
 			 * (rather than being silently ignored/discarded as IMAP_CLIENT_EXPECT_EVENTUALLY does).
 			 * This way, the client can stay synchronized with the remote server, which is important
-			 * since APPEND can trigger untagged EXISTS's, etc. */
-			if (imap_client_wait_response(destclient, -1, SEC_MS(5))) {
+			 * since APPEND can trigger untagged EXISTS's, etc.
+			 *
+			 * At the same time, we don't want to relay the tagged response! */
+			if (imap_client_wait_response_noechotag(destclient, -1, SEC_MS(5))) {
 				goto cleanup;
 			}
 		}
@@ -4391,6 +4393,8 @@ static int imap_process(struct imap_session *imap, char *s)
 	int replacecount;
 	char *command = NULL; /* XXX Should not need to be initialized, but gcc 12 complains if it's not */
 	int res = 0;
+
+	imap->finalized_response = 0; /* New command, reset */
 
 	if (imap->idle || (imap->alerted == 1 && !strcasecmp(s, "DONE"))) {
 		/* Thunderbird clients will still send "DONE" if we send a tagged reply during the IDLE,

nets/net_imap/imap.h
@@ -89,12 +89,13 @@ struct imap_session {
 	unsigned int readonly:1;	/* SELECT vs EXAMINE */
 	unsigned int inauth:1;
 	unsigned int idle:1;		/* Whether IDLE is active */
-	unsigned int dnd:1;			/* Do Not Disturb: Whether client is executing a FETCH, STORE, or SEARCH command (EXPUNGE responses are not allowed) */
+	unsigned int dnd:1;			/* Do Not Disturb: Whether client is executing a FETCH, STORE, or SEARCH command (EXPUNGE responses are not allowed). XXX Unused? */
 	unsigned int pending:1;		/* Delayed output is pending in pfd pipe */
 	unsigned int expungepending:1;	/* EXPUNGE updates pending in pipe */
 	unsigned int alerted:2;		/* An alert has been delivered to this client */
 	unsigned int condstore:1;	/* Whether a client has issue a CONDSTORE enabling command, and should be sent MODSEQ updates in untagged FETCH responses */
 	unsigned int qresync:1;		/* Whether a client has enabled the QRESYNC capability */
+	unsigned int finalized_response:1;	/* Whether tagged response to this command has been sent already */
 	struct imap_notify *notify;	/* NOTIFY events */
 	bbs_mutex_t lock;		/* Lock for IMAP session */
 	RWLIST_ENTRY(imap_session) entry;	/* Next active session */
@@ -177,7 +178,11 @@ extern int imap_debug_level;
 #define _imap_reply(imap, fmt, ...) _imap_reply_nolock(imap, fmt, ## __VA_ARGS__)
 #define imap_send_nocrlf(imap, fmt, ...) _imap_reply_nolock_fd_lognewline(imap, imap->node->wfd, fmt, ## __VA_ARGS__);
 #define imap_send(imap, fmt, ...) _imap_reply(imap, "%s " fmt "\r\n", "*", ## __VA_ARGS__)
-#define imap_reply(imap, fmt, ...) _imap_reply(imap, "%s " fmt "\r\n", imap->tag, ## __VA_ARGS__)
+#define imap_reply(imap, fmt, ...) do { \
+	bbs_assert(!imap->finalized_response); \
+	imap->finalized_response = 1; \
+	_imap_reply(imap, "%s " fmt "\r\n", imap->tag, ## __VA_ARGS__); \
+} while (0);
 
 #define imap_parallel_send(imap, fmt, ...) _imap_parallel_reply(imap, "%s " fmt "\r\n", "*", ## __VA_ARGS__)
 

nets/net_imap/imap_client.c
@@ -482,6 +482,7 @@ static ssize_t client_command_passthru(struct imap_client *client, int fd, const
 
 	for (;;) {
 		int cbres = 0;
+		int is_tagged_response, do_echo;
 		char *buf = client->buf;
 		if (fd != -1) {
 			res = bbs_multi_poll(pfds, 2, ms); /* If returns 1, client->rfd had activity, if 2, it was fd */
@@ -517,7 +518,12 @@ static ssize_t client_command_passthru(struct imap_client *client, int fd, const
 				break;
 			}
 		}
-		if (echo) {
+		/* If echo is 0, don't echo (relay back to local client)
+		 * If 1, unconditionally echo.
+		 * If 2, echo as long as it is not the tagged response. */
+		is_tagged_response = !strncmp(buf, tag, (size_t) taglen);
+		do_echo = echo == 1 || (echo == 2 && !is_tagged_response);
+		if (do_echo) {
 			/* Go ahead and relay it */
 			if (res > 0) { /* If it was just an empty line, don't bother calling write() with 0 bytes */
 				bbs_write(imap->node->wfd, buf, (unsigned int) res);
@@ -527,16 +533,22 @@ static ssize_t client_command_passthru(struct imap_client *client, int fd, const
 				break;
 			}
 		}
+		/* Allow easily differentiating replies that we silently discard and those passed through
+		 * <= if passed through, <- if not */
 #ifdef DEBUG_REMOTE_RESPONSES
 		/* NEVER enable this in production because this will be a huge volume of data */
-		imap_debug(10, "<= %.*s\n", (int) res, buf);
+		imap_debug(10, "<%c %.*s\n", do_echo ? '=' : '-', (int) res, buf);
 #else
 		if (c++ < 15 && res > 2 && !strncmp(buf, "* ", STRLEN("* "))) {
-			imap_debug(7, "<= %.*s\n", (int) res, buf);
+			imap_debug(7, "<%c %.*s\n", do_echo ? '=' : '-', (int) res, buf);
 		}
 #endif
-		if (!strncmp(buf, tag, (size_t) taglen)) {
-			imap_debug(10, "<= %.*s\n", (int) res, buf);
+		if (is_tagged_response) {
+			if (do_echo) {
+				/* We just relayed the tagged response from the remote server to local client */
+				imap->finalized_response = 1;
+			}
+			imap_debug(10, "<%c %.*s\n", do_echo ? '=' : '-', (int) res, buf);
 			if (!STARTS_WITH(buf + taglen, "OK")) { /* BAD or NO */
 				/* We did something we shouldn't have, oops */
 				if (cmd) {

nets/net_imap/imap_client.h
@@ -93,6 +93,8 @@ ssize_t __attribute__ ((format (gnu_printf, 3, 4))) __imap_client_send_log(struc
 #define imap_client_send_log(client, fmt, ...) __imap_client_send_log(client, 1, fmt, ## __VA_ARGS__)
 
 #define imap_client_wait_response(client, fd, ms) __imap_client_wait_response(client, fd, ms, 1, __LINE__, NULL, NULL)
+#define imap_client_wait_response_noecho(client, fd, ms) __imap_client_wait_response(client, fd, ms, 0, __LINE__, NULL, NULL)
+#define imap_client_wait_response_noechotag(client, fd, ms) __imap_client_wait_response(client, fd, ms, 2, __LINE__, NULL, NULL)
 
 #define imap_client_send_wait_response(client, fd, ms, fmt, ...) __imap_client_send_wait_response(client, fd, ms, 1, __LINE__, NULL, NULL, fmt, ## __VA_ARGS__)
 #define imap_client_send_wait_response_cb(client, fd, ms, cb, cbdata, fmt, ...) __imap_client_send_wait_response(client, fd, ms, 1, __LINE__, cb, cbdata, fmt, ## __VA_ARGS__)
@@ -107,7 +109,7 @@ int __imap_client_wait_response(struct imap_client *client, int fd, int ms, int 
  * \param client
  * \param fd Additional file descriptor on which to poll(used only for IDLE, -1 otherwise)
  * \param ms Max time to wait for poll / bbs_readline
- * \param echo Whether to relay output from remote server to our local client
+ * \param echo 1 to relay output from remote server to our local client, 2 to relay output except tagged repsonse, 0 to not relay any output
  * \param lineno
  * \param cb Custom callback to run for each line received from remote server. Returning -1 from callback will terminate wait loop
  * \param cbdata Callback data for callback function