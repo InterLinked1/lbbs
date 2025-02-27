InterLinked1/lbbs

commit 0ea87e331cfab83b8a263093b21b3da7fe734b8d

io_tls: Defer rebuilds of TLS session list.
When a TLS session ends, the entire TLS session list needs
to rebuilt so as not to exclude the session that just ended.
However, in the case of many sessions ending simultaneously,
this could be inefficient as the list would be repeatedly
for each destroyed session only to be immediately rebuilt
following the next termination. Instead, when a session
is destroyed, wait a brief amount of time for any additional
sessions to terminate, and keep waiting as long as sessions
are actively being destroyed, up to a point, at which time
the session list is rebuilt regardless. This ensures that
"starvation" of remaining active sessions cannot occur due to
them not being serviced, while eliminating most unnecessary
consecutive rebuilds of the session list.

io/io_tls.c
@@ -243,6 +243,21 @@ static void ssl_fd_free(struct ssl_fd *sfd)
 }
 
 static int needcreate = 1; /* Whether the list of TLS sessions is stale and needs to be rebuilt */
+static int defer_rebuild = 0; /* Whether to defer the next rebuild of the session list */
+
+/*! \note It's possible it might not be in the list, because the owner thread has already called ssl_unregister_fd,
+ * in which case there's no need to do anything. */
+#define MARK_DEAD(s) \
+	RWLIST_TRAVERSE(&sslfds, sfd, entry) { \
+		if (sfd->ssl == s) { \
+			if (sfd->dead) { /* Shouldn't happen since we rebuild traversal list after MARK_DEAD */ \
+				bbs_warning("SSL connection %p already marked as dead?\n", sfd->ssl); \
+			} \
+			sfd->dead = 1; \
+			bbs_debug(5, "SSL connection %p now marked as dead\n", sfd->ssl); \
+			break; \
+		} \
+	} \
 
 static int ssl_unregister_fd(SSL *ssl)
 {
@@ -253,7 +268,9 @@ static int ssl_unregister_fd(SSL *ssl)
 	if (sfd) {
 		/* Any list of SSL sessions is now stale, since it
 		 * could potentially include the session we just removed. */
+		MARK_DEAD(ssl);
 		needcreate = 1;
+		defer_rebuild = 1;
 	}
 	RWLIST_UNLOCK(&sslfds);
 	if (sfd) {
@@ -282,20 +299,6 @@ static void ssl_cleanup_fds(void)
 }
 
 static pthread_t ssl_thread;
-
-/*! \note It's possible it might not be in the list, because the owner thread has already called ssl_unregister_fd,
- * in which case there's no need to do anything. */
-#define MARK_DEAD(s) \
-	RWLIST_TRAVERSE(&sslfds, sfd, entry) { \
-		if (sfd->ssl == s) { \
-			if (sfd->dead) { /* Shouldn't happen since we rebuild traversal list after MARK_DEAD */ \
-				bbs_warning("SSL connection %p already marked as dead?\n", sfd->ssl); \
-			} \
-			sfd->dead = 1; \
-			bbs_debug(5, "SSL connection %p now marked as dead\n", sfd->ssl); \
-			break; \
-		} \
-	} \
 
 /*! \brief Dump TLS sessions */
 static int cli_tls(struct bbs_cli_args *a)
@@ -353,6 +356,8 @@ static void *ssl_io_thread(void *unused)
 	char buf[8192];
 	int pending;
 	int inovertime = 0, overtime = 0, num_deferred_writes = 0;
+#define MAX_DEFERRED_REBUILDS 12
+	int max_deferred_rebuilds = MAX_DEFERRED_REBUILDS; /* Maximum number of rebuilds that can be deferred before we rebuild no matter what */
 	char err_msg[1024];
 
 	UNUSED(unused);
@@ -362,10 +367,32 @@ static void *ssl_io_thread(void *unused)
 	/* Only recreate pfds when we read from the alertpipe, otherwise, it's the same file descriptors the next round */
 	for (;;) {
 		if (needcreate) {
+			/* Because some types of connections involve many TLS sessions (e.g. a mail session with client proxies to many other servers),
+			 * when the parent connection gets torn down, we could have many TLS connections all being torn down at once.
+			 * Naively, we might rebuild the TLS session list as each one is torn down, all within ms of each other,
+			 * but this is clearly inefficient.
+			 * As an optimization, before rebuilding the list, wait a few ms for something else to happen. */
 			int numdead = 0;
 			if (ssl_shutting_down) {
 				bbs_debug(4, "SSL I/O thread has been instructed to exit\n");
 				break; /* We're shutting down. */
+			}
+			while (defer_rebuild) {
+				defer_rebuild = 0; /* Not protected by lock, but that's okay */
+				if (!max_deferred_rebuilds--) {
+					max_deferred_rebuilds = MAX_DEFERRED_REBUILDS;
+					bbs_debug(8, "Maximum deferred rebuilds reached, rebuilding immediately\n");
+					break;
+				}
+				bbs_debug(8, "Temporarily deferring rebuild of TLS session list\n");
+				/* If another session needs servicing, ideally we should stop waiting and handle it immediately.
+				 * However, polling with the existing pfds here won't be accurate,
+				 * since the session that ended originally still has activity on it.
+				 * We could update the poll list so it's accurate, but that is precisely the work we are trying to avoid here.
+				 * So just sleep for a very short period of time that is unlikely to disrupt any active sessions. */
+				if (bbs_safe_sleep(2)) { /* Wait 2 ms */
+					break;
+				}
 			}
 			bbs_debug(8, "Rebuilding TLS session list\n");
 			needcreate = 0;

commit 28c0194e8de8e0ab7e4a1111324c4807d76a447d

io_tls: Fix file descriptor leak on cleanup.
Commit 0ea87e331cfab83b8a263093b21b3da7fe734b8d introduced a regression
which leaked file descriptors for TLS sessions by adding a call to the
MARK_DEAD macro. This macro modifies the sfd variable in place, which
was not a problem in the TLS I/O thread, but would result in sfd being
NULL post-traversal in ssl_unregister_fd, causing the TLS session to
not be cleaned up. So:

* Use a local variable for the traversal in MARK_DEAD to avoid mutating
  a variable outside the macro.
* Log a warning if unregistering fails and we return -1. We were returning
  -1 previously, but there was no warning or error logged when this happened.
* Simplify some cleanup code by using PIPE_CLOSE where possible.

io/io_tls.c
@@ -214,8 +214,7 @@ static int ssl_register_fd(SSL *ssl, int fd, int *rfd, int *wfd, int client)
 		return -1;
 	} else if (pipe(sfd->writepipe)) {
 		bbs_error("pipe failed: %s\n", strerror(errno));
-		close(sfd->readpipe[0]);
-		close(sfd->readpipe[1]);
+		PIPE_CLOSE(sfd->readpipe);
 		free(sfd);
 		RWLIST_UNLOCK(&sslfds);
 		return -1;
@@ -235,10 +234,8 @@ static int ssl_register_fd(SSL *ssl, int fd, int *rfd, int *wfd, int client)
 
 static void ssl_fd_free(struct ssl_fd *sfd)
 {
-	close(sfd->readpipe[1]);
-	close(sfd->readpipe[0]);
-	close(sfd->writepipe[1]);
-	close(sfd->writepipe[0]);
+	PIPE_CLOSE(sfd->readpipe);
+	PIPE_CLOSE(sfd->writepipe);
 	free(sfd);
 }
 
@@ -247,17 +244,19 @@ static int defer_rebuild = 0; /* Whether to defer the next rebuild of the sessio
 
 /*! \note It's possible it might not be in the list, because the owner thread has already called ssl_unregister_fd,
  * in which case there's no need to do anything. */
-#define MARK_DEAD(s) \
-	RWLIST_TRAVERSE(&sslfds, sfd, entry) { \
-		if (sfd->ssl == s) { \
-			if (sfd->dead) { /* Shouldn't happen since we rebuild traversal list after MARK_DEAD */ \
-				bbs_warning("SSL connection %p already marked as dead?\n", sfd->ssl); \
+#define MARK_DEAD(s) { \
+	struct ssl_fd *sfd_traverse = sfd; /* Don't modify sfd in place or it would be NULL after traversal */ \
+	RWLIST_TRAVERSE(&sslfds, sfd_traverse, entry) { \
+		if (sfd_traverse->ssl == s) { \
+			if (sfd_traverse->dead) { /* Shouldn't happen since we rebuild traversal list after MARK_DEAD */ \
+				bbs_warning("SSL connection %p already marked as dead?\n", sfd_traverse->ssl); \
 			} \
-			sfd->dead = 1; \
-			bbs_debug(5, "SSL connection %p now marked as dead\n", sfd->ssl); \
+			sfd_traverse->dead = 1; \
+			bbs_debug(5, "SSL connection %p now marked as dead\n", sfd_traverse->ssl); \
 			break; \
 		} \
 	} \
+}
 
 static int ssl_unregister_fd(SSL *ssl)
 {
@@ -278,6 +277,7 @@ static int ssl_unregister_fd(SSL *ssl)
 		bbs_alertpipe_write(ssl_alert_pipe); /* Notify I/O thread that we removed a fd, although it'll probably detect this anyways. */
 		return 0;
 	}
+	bbs_warning("Couldn't unregister SSL session for %p?\n", ssl);
 	return -1;
 }
 
@@ -1244,7 +1244,7 @@ static int tlsreload(int fd)
 	}
 
 	ssl_is_available = 0; /* Ensure any new connections are rejected until we're done reloading. */
-	RWLIST_UNLOCK(&sslfds); /* tls_cleanup will lock the list again, so unlock it for now. */
+	RWLIST_UNLOCK(&sslfds); /* tls_cleanup will lock the list again, so unlock it for now. XXX It's a different lock though, so could unlock after? */
 
 	tls_cleanup();
 