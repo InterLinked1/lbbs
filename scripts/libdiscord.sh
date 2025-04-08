39ae96d374c157ba9dc35059877e405d4972335f
InterLinked1 <24227567+InterLinked1@users.noreply.github.com>
2025-04-08 13:45:25 -0400

    libdiscord: Compile curl from source if needed.
    As of libdiscord commit https://github.com/Cogmasters/concord/commit/4ae21178aad8d8da29cc918d8af36b5e0a6d3b27
    libdiscord now requires curl version >= 7.86.0 for Websocket support,
    so build curl from source if needed.

--- scripts/libdiscord.sh
+++ scripts/libdiscord.sh
@@ -4,6 +4,37 @@
 
 set -e
 
+# libcurl 7.86.0 or higher is required
+curlv=$( curl-config --version | cut -d' ' -f2 | tr -d '\n' )
+if [ $? -eq 0 ]; then
+	printf "$curlv\n7.86.0" | sort -V | head -n1 | grep -qF 7.86.0 && ret=$? || ret=$? # capture failure since we have set -e
+	if [ $ret -ne 0 ]; then
+		printf "libcurl $curlv (< 7.86.0) is currently installed, need to build from source\n"
+		CURL_SRC_VER="8.13.0"
+	else
+		printf "libcurl $curlv (>= 7.86.0) is currently installed, package okay\n"
+	fi
+else
+	printf "libcurl is not currently installed?\n"
+	CURL_SRC_VER="8.13.0"
+fi
+
+if [ "$CURL_SRC_VER" != "" ]; then
+	# Based on https://github.com/Cogmasters/concord/commit/04027977b6e1fe06e2d1ff589edb425223197046
+	cd /usr/local/src
+	if [ -f curl-${CURL_SRC_VER}.tar.gz ]; then
+		rm curl-${CURL_SRC_VER}.tar.gz
+	fi
+	wget https://curl.se/download/curl-${CURL_SRC_VER}.tar.gz
+	tar -xzf curl-${CURL_SRC_VER}.tar.gz
+	# libpsl doesn't seem to be available on all distros (e.g. Rocky Linux 8.9)
+	# https://daniel.haxx.se/blog/2024/01/10/psl-in-curl/
+	cd curl-${CURL_SRC_VER} && ./configure --with-openssl --enable-websockets --without-libpsl
+	make -j$(nproc)
+	make install
+	ldconfig
+fi
+
 cd /usr/local/src
 if [ ! -d concord ]; then
 	git clone https://github.com/cogmasters/concord.git
