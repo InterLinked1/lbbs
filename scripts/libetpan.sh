c3b497464057db2558291bc694ed3cc38b795460
InterLinked1 <24227567+InterLinked1@users.noreply.github.com>
2025-09-16 16:39:04 -0400

    libetpan: Slipstream unmerged bug fix.
    Since libetpan is not being maintained, slipstream a bug fix submitted
    upstream for it during the build process when installing prereqs.

--- scripts/libetpan.sh
+++ scripts/libetpan.sh
@@ -17,10 +17,18 @@ else
 	# but the Makefile hasn't yet been generated
 	make clean || git reset --hard origin/master
 fi
+
+# Slipstream patches needed for LBBS net_imap functionality
 wget "https://github.com/dinhvh/libetpan/commit/5ea630e6482422ffa2e26b9afe5fb47a9eb673a2.diff"
 wget "https://github.com/dinhvh/libetpan/commit/4226610e3dc19f58345ae7c5146fa8cf249ca97b.patch"
 git apply "5ea630e6482422ffa2e26b9afe5fb47a9eb673a2.diff" # IMAP STATUS=SIZE
 git apply "4226610e3dc19f58345ae7c5146fa8cf249ca97b.patch" # SMTP AUTH
+
+# Slipstream other bug fixes, since libetpan is not currently being maintained upstream
+# Don't you just love unmaintained open source software?
+wget "https://patch-diff.githubusercontent.com/raw/dinhvh/libetpan/pull/447.diff"
+git apply 447.diff
+
 ./autogen.sh --with-poll
 make
 make install
