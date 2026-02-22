InterLinked1/lbbs

commit 7e8e91f7d9746e48838bbdbed001503812d07e9d

module.c: Ensure that 'noload' is obeyed for dependencies.
Due to the new autoloading logic (using a single linked list, instead of
separate stringlists), if a module is marked for autoload, but another
module that we want to load depends on it, the noload would be ignored
and the module would be loaded anyways. We now check if the dependency
(or any of its dependencies, in turn) was noloaded, to ensure we don't
override this.

bbs/module.c
@@ -431,6 +431,18 @@ static struct autoload_module *find_autoload_module(const char *module)
 	return NULL;
 }
 
+#ifndef DLOPEN_ONLY_ONCE
+static int dependency_noloaded(const char *name)
+{
+	struct autoload_module *a = find_autoload_module(name);
+	if (a) {
+		return a->noload;
+	}
+	bbs_debug(5, "Couldn't find '%s' in autoload list?\n", name);
+	return 0;
+}
+#endif
+
 /* Forward declaration */
 static int load_resource(struct autoload_module *a, const char *restrict resource_name, unsigned int suppress_logging);
 
@@ -508,6 +520,12 @@ static struct bbs_module *load_dynamic_module(struct autoload_module *a, const c
 								res = -1;
 								break;
 							}
+						}
+						if (dependency_noloaded(d->name)) {
+							/* Either the dependency or another module upon which the dependency is dependent is noloaded */
+							bbs_error("Module %s is dependent on %s, which cannot be loaded\n", a->name, d->name);
+							res = -1;
+							break;
 						}
 						bbs_debug(1, "Preloading %s on the fly since it's required by %s\n", dependency, resource_in);
 						/* Since we automatically reorder modules with dependencies for autoload,

commit 34a36851c5f1f3d0e3d2b1baa1c3e423fa4aeb18

module.c: Fix possible NULL pointer dereference.
Fix regression introduced by commit 7e8e91f7d9746e48838bbdbed001503812d07e9d.
The autoload module object can be NULL, so we need to check before using it.

bbs/module.c
@@ -520,12 +520,12 @@ static struct bbs_module *load_dynamic_module(struct autoload_module *a, const c
 								res = -1;
 								break;
 							}
-						}
-						if (dependency_noloaded(d->name)) {
-							/* Either the dependency or another module upon which the dependency is dependent is noloaded */
-							bbs_error("Module %s is dependent on %s, which cannot be loaded\n", a->name, d->name);
-							res = -1;
-							break;
+							if (d && dependency_noloaded(d->name)) {
+								/* Either the dependency or another module upon which the dependency is dependent is noloaded */
+								bbs_error("Module %s is dependent on %s, which is noloaded\n", a->name, d->name);
+								res = -1;
+								break;
+							}
 						}
 						bbs_debug(1, "Preloading %s on the fly since it's required by %s\n", dependency, resource_in);
 						/* Since we automatically reorder modules with dependencies for autoload,