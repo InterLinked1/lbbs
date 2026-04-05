80d59c2912c8ce3639e42e33235df82e2a240dd9
InterLinked1 <24227567+InterLinked1@users.noreply.github.com>
2026-04-05 13:42:41 -0400

    libdiscord: Slipstream unmerged patch.
    Slipstream a patch needed to prevent crashes due to double frees.

--- scripts/libdiscord.sh
+++ scripts/libdiscord.sh
@@ -78,6 +78,10 @@ else
 	make clean
 fi
 
+# Apply critical fix to prevent double free
+wget "https://patch-diff.githubusercontent.com/raw/Cogmasters/concord/pull/229.diff"
+git apply 229.diff
+
 printf "Compiling libdiscord\n"
 CFLAGS="-fPIC" make shared -j$(nproc)
 make install
