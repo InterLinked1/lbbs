d15c473785ea11bea610507ed88dbf32415b4d77
InterLinked1 <24227567+InterLinked1@users.noreply.github.com>
2026-05-19 09:46:49 -0400

    libetpan: Remove merged patches.
    libetpan is being maintained again and has merged most of the patches
    that were previously being slipstreamed.

--- scripts/libetpan.sh
+++ scripts/libetpan.sh
@@ -19,15 +19,8 @@ else
 fi
 
 # Slipstream patches needed for LBBS net_imap functionality
-wget "https://github.com/dinhvh/libetpan/commit/5ea630e6482422ffa2e26b9afe5fb47a9eb673a2.diff"
 wget "https://github.com/dinhvh/libetpan/commit/4226610e3dc19f58345ae7c5146fa8cf249ca97b.patch"
-git apply "5ea630e6482422ffa2e26b9afe5fb47a9eb673a2.diff" # IMAP STATUS=SIZE
 git apply "4226610e3dc19f58345ae7c5146fa8cf249ca97b.patch" # SMTP AUTH
-
-# Slipstream other bug fixes, since libetpan is not currently being maintained upstream
-# Don't you just love unmaintained open source software?
-wget "https://patch-diff.githubusercontent.com/raw/dinhvh/libetpan/pull/447.diff"
-git apply 447.diff
 
 ./autogen.sh --with-poll > /dev/null || ./autogen.sh --with-poll
 
@@ -39,7 +32,7 @@ if [ "$ostype" = "FreeBSD" ]; then
 	MAKE=gmake
 fi
 
-# libetpan's compilation is quite verbose, and full of warnings we can't do anything about since it's not being maintained
+# libetpan's compilation is quite verbose, and full of warnings we can't do anything about
 # Suppress most output unless something goes wrong
 $MAKE -j$(nproc) >/dev/null 2>&1 || $MAKE # Quiet, but show errors on failure
 $MAKE install
